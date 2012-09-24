113a4654f163658bfd734cdf6ab9c67b3b8c1690
Kevin Miller <kemiller@csumb.edu>
2012-09-24 08:48:34 -0700

    added language code standard.

--- src/quail.js
+++ src/quail.js
@@ -427,6 +427,17 @@
       });
     },
     
+    documentLangIsISO639Standard : function() {
+      var languages = quail.loadString('language_codes');
+      var language = quail.html.find('html').attr('lang');
+      if(!language) {
+        return;
+      }
+      if(languages.indexOf(language) === -1) {
+          quail.accessibilityResults.documentLangIsISO639Standard.push(quail.html.find('html'));
+      }
+    },
+    
     doctypeProvided : function() {
       if(!document.doctype) {
         quail.accessibilityResults.doctypeProvided.push(quail.html.find('html'));
