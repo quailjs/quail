90dd780def87c3db5504614a6b114dfabfc837d3
Kevin Miller <kemiller@csumb.edu>
2012-10-29 07:09:37 -0700

    Fixed some indentation issues.

--- src/quail.js
+++ src/quail.js
@@ -913,81 +913,81 @@
     }
   };
 
-quail.colors = {
+  quail.colors = {
   
-  getLuminosity : function(foreground, background) {
-    foreground = quail.colors.cleanup(foreground);
-    background = quail.colors.cleanup(background);
-    var RsRGB = foreground.r/255;
-    var GsRGB = foreground.g/255;
-    var BsRGB = foreground.b/255;
-    var R = (RsRGB <= 0.03928) ? RsRGB/12.92 : Math.pow((RsRGB+0.055)/1.055, 2.4);
-    var G = (GsRGB <= 0.03928) ? GsRGB/12.92 : Math.pow((GsRGB+0.055)/1.055, 2.4);
-    var B = (BsRGB <= 0.03928) ? BsRGB/12.92 : Math.pow((BsRGB+0.055)/1.055, 2.4);
-
-    var RsRGB2 = background.r/255;
-    var GsRGB2 = background.g/255;
-    var BsRGB2 = background.b/255;
-    var R2 = (RsRGB2 <= 0.03928) ? RsRGB2/12.92 : Math.pow((RsRGB2+0.055)/1.055, 2.4);
-    var G2 = (GsRGB2 <= 0.03928) ? GsRGB2/12.92 : Math.pow((GsRGB2+0.055)/1.055, 2.4);
-    var B2 = (BsRGB2 <= 0.03928) ? BsRGB2/12.92 : Math.pow((BsRGB2+0.055)/1.055, 2.4);
-    var l1, l2;
-    if (foreground.r + foreground.g + foreground.b <= background.r + background.g + background.b) {
-      l2 = (0.2126 * R + 0.7152 * G + 0.0722 * B);
-      l1 = (0.2126 * R2 + 0.7152 * G2 + 0.0722 * B2);
-    } else {
-      l1 = (0.2126 * R + 0.7152 * G + 0.0722 * B);
-      l2 = (0.2126 * R2 + 0.7152 * G2 + 0.0722 * B2);
-    }
-
-    return Math.round((l1 + 0.05)/(l2 + 0.05),2);
-  },
+    getLuminosity : function(foreground, background) {
+      foreground = quail.colors.cleanup(foreground);
+      background = quail.colors.cleanup(background);
+      var RsRGB = foreground.r/255;
+      var GsRGB = foreground.g/255;
+      var BsRGB = foreground.b/255;
+      var R = (RsRGB <= 0.03928) ? RsRGB/12.92 : Math.pow((RsRGB+0.055)/1.055, 2.4);
+      var G = (GsRGB <= 0.03928) ? GsRGB/12.92 : Math.pow((GsRGB+0.055)/1.055, 2.4);
+      var B = (BsRGB <= 0.03928) ? BsRGB/12.92 : Math.pow((BsRGB+0.055)/1.055, 2.4);
   
-  passesWCAG : function(element) {
-    return (quail.colors.getLuminosity(quail.colors.getColor(element, 'foreground'), quail.colors.getColor(element, 'background')) > 5);
-  },
+      var RsRGB2 = background.r/255;
+      var GsRGB2 = background.g/255;
+      var BsRGB2 = background.b/255;
+      var R2 = (RsRGB2 <= 0.03928) ? RsRGB2/12.92 : Math.pow((RsRGB2+0.055)/1.055, 2.4);
+      var G2 = (GsRGB2 <= 0.03928) ? GsRGB2/12.92 : Math.pow((GsRGB2+0.055)/1.055, 2.4);
+      var B2 = (BsRGB2 <= 0.03928) ? BsRGB2/12.92 : Math.pow((BsRGB2+0.055)/1.055, 2.4);
+      var l1, l2;
+      if (foreground.r + foreground.g + foreground.b <= background.r + background.g + background.b) {
+        l2 = (0.2126 * R + 0.7152 * G + 0.0722 * B);
+        l1 = (0.2126 * R2 + 0.7152 * G2 + 0.0722 * B2);
+      } else {
+        l1 = (0.2126 * R + 0.7152 * G + 0.0722 * B);
+        l2 = (0.2126 * R2 + 0.7152 * G2 + 0.0722 * B2);
+      }
   
-  passesWAI : function(element) {
-    var foreground = quail.colors.cleanup(quail.colors.getColor(element, 'foreground'));
-    var background = quail.colors.cleanup(quail.colors.getColor(element, 'background'));
-    return (quail.colors.getWAIErtContrast(foreground, background) > 500 &&
-            quail.colors.getWAIErtBrightness(foreground, background) > 125);
-  },
+      return Math.round((l1 + 0.05)/(l2 + 0.05),2);
+    },
+    
+    passesWCAG : function(element) {
+      return (quail.colors.getLuminosity(quail.colors.getColor(element, 'foreground'), quail.colors.getColor(element, 'background')) > 5);
+    },
+    
+    passesWAI : function(element) {
+      var foreground = quail.colors.cleanup(quail.colors.getColor(element, 'foreground'));
+      var background = quail.colors.cleanup(quail.colors.getColor(element, 'background'));
+      return (quail.colors.getWAIErtContrast(foreground, background) > 500 &&
+              quail.colors.getWAIErtBrightness(foreground, background) > 125);
+    },
+    
+    getWAIErtContrast : function(foreground, background) {
+      var diffs = quail.colors.getWAIDiffs(foreground, background);
+      return diffs.red + diffs.green + diffs.blue;
+    },
   
-  getWAIErtContrast : function(foreground, background) {
-    var diffs = quail.colors.getWAIDiffs(foreground, background);
-    return diffs.red + diffs.green + diffs.blue;
-  },
-
-  getWAIErtBrightness : function(foreground, background) {
-    var diffs = quail.colors.getWAIDiffs(foreground, background);
-    return ((diffs.red * 299) + (diffs.green * 587) + (diffs.blue * 114)) / 1000;
-
-  },
+    getWAIErtBrightness : function(foreground, background) {
+      var diffs = quail.colors.getWAIDiffs(foreground, background);
+      return ((diffs.red * 299) + (diffs.green * 587) + (diffs.blue * 114)) / 1000;
   
-  getWAIDiffs : function(foreground, background) {
-     var diff = { };
-     diff.red = Math.abs(foreground.r - background.r); 
-     diff.green = Math.abs(foreground.g - background.g);
-     diff.blue = Math.abs(foreground.b - background.b);
-     return diff;
-  },
-  
-  getColor : function(element, type) {
-    if(type === 'foreground') {
-      return (element.css('color')) ? element.css('color') : 'rgb(255,255,255)';
-    }
-    return (element.css('background-color')) ? element.css('background-color') : 'rgb(0,0,0)';
-  },
-  
-  cleanup : function(color) {
-    color = color.replace('rgb(', '').replace('rgba(', '').replace(')', '').split(',');
-    return { r : color[0],
-             g : color[1],
-             b : color[2],
-             a : ((typeof color[3] === 'undefined') ? false : color[3])
-           };
-  }
+    },
+    
+    getWAIDiffs : function(foreground, background) {
+       var diff = { };
+       diff.red = Math.abs(foreground.r - background.r); 
+       diff.green = Math.abs(foreground.g - background.g);
+       diff.blue = Math.abs(foreground.b - background.b);
+       return diff;
+    },
+    
+    getColor : function(element, type) {
+      if(type === 'foreground') {
+        return (element.css('color')) ? element.css('color') : 'rgb(255,255,255)';
+      }
+      return (element.css('background-color')) ? element.css('background-color') : 'rgb(0,0,0)';
+    },
+    
+    cleanup : function(color) {
+      color = color.replace('rgb(', '').replace('rgba(', '').replace(')', '').split(',');
+      return { r : color[0],
+               g : color[1],
+               b : color[2],
+               a : ((typeof color[3] === 'undefined') ? false : color[3])
+             };
+  };
   
 };
 
