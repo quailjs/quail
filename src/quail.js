91135c8d5dea0bd47c1086152e326e38244eb630
Kevin Miller <kemiller@csumb.edu>
2012-11-07 07:23:13 -0800

    Made test for alt is differnt than source also just check file name at end of path.

--- src/quail.js
+++ src/quail.js
@@ -597,7 +597,7 @@
 
     imgAltIsDifferent : function() {
       quail.html.find('img[alt][src]').each(function() {
-        if($(this).attr('src') === $(this).attr('alt')) {
+        if($(this).attr('src') === $(this).attr('alt') || $(this).attr('src').split('/').pop() === $(this).attr('alt')) {
           quail.testFails('imgAltIsDifferent', $(this));
         }
       });
