fba9bb43e13fdaa5192d81e448f3cbe1482b9a04
Kevin Miller <kemiller@csumb.edu>
2013-02-14 18:20:49 -0800

    Added back test for OAC 197 per #20.

--- src/quail.js
+++ src/quail.js
@@ -528,7 +528,11 @@
     aSuspiciousLinkText : function() {
       var suspiciousText = quail.loadString('suspicious_links');
       quail.html.find('a').each(function() {
-        if(suspiciousText.indexOf(quail.cleanString($(this).text())) > -1) {
+        var text = $(this).text();
+        $(this).find('img[alt]').each(function() {
+          text = text + $(this).attr('alt');
+        });
+        if(suspiciousText.indexOf(quail.cleanString(text)) > -1) {
           quail.testFails('aSuspiciousLinkText', $(this));
         }
       });
