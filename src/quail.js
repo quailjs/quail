ef27627e7662a9365ea78384e335d23b6b246c31
Kevin Miller <kemiller@csumb.edu>
2013-02-15 21:16:32 -0800

    Added OAC tests 204-208, 236 per #20

--- src/quail.js
+++ src/quail.js
@@ -405,14 +405,18 @@
 
     labelTest : function(testName, options) {
       quail.html.find(options.selector).each(function() {
-        if(!$(this).parent('label').length &&
-          !quail.html.find('label[for=' + $(this).attr('id') + ']').length) {
+        if((!$(this).parent('label').length ||
+            !quail.containsReadableText($(this).parent('label'))) &&
+          (!quail.html.find('label[for=' + $(this).attr('id') + ']').length ||
+           !quail.containsReadableText(quail.html.find('label[for=' + $(this).attr('id') + ']')))) {
             quail.testFails(testName, $(this));
         }
       });
     },
     
     containsReadableText : function(element, children) {
+      element = element.clone();
+      element.find('option').remove();
       if(!quail.isUnreadable(element.text())) {
         return true;
       }
