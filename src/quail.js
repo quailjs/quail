3e8506de146d054a5f43fa256ac8efb52f2fc608
Kevin Miller <kemiller@csumb.edu>
2013-05-28 16:36:38 -0700

    Made aMustContainText ignore anchors with names or ids. Fixes #35

--- src/quail.js
+++ src/quail.js
@@ -518,7 +518,8 @@
     
     aMustContainText : function() {
       quail.html.find('a').each(function() {
-        if(!quail.containsReadableText($(this), true) && !($(this).attr('name') && !$(this).attr('href'))) {
+        if(!quail.containsReadableText($(this), true) && 
+           !(($(this).attr('name') || $(this).attr('id')) && !$(this).attr('href'))) {
           quail.testFails('aMustContainText', $(this));
         }
       });
