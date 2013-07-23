e697466a0779d11d47eeeaf8806404aa08227630
Kevin Miller <kemiller@csumb.edu>
2013-07-23 11:16:13 -0700

    Wrapped pxToEm plugin in closure.

--- libs/jquery.pxToEm/pxem.jQuery.js
+++ libs/jquery.pxToEm/pxem.jQuery.js
@@ -8,26 +8,27 @@
  		scope: string or jQuery selector for font-size scoping		  
  * Usage Example: $(myPixelValue).toEm(); or $(myEmValue).toPx();
 --------------------------------------------------------------------*/
+(function($) {
+	$.fn.toEm = function(settings){
+		settings = jQuery.extend({
+			scope: 'body'
+		}, settings);
+		var that = parseInt(this[0],10),
+			scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
+			scopeVal = scopeTest.height();
+		scopeTest.remove();
+		return (that / scopeVal).toFixed(8) + 'em';
+	};
 
-$.fn.toEm = function(settings){
-	settings = jQuery.extend({
-		scope: 'body'
-	}, settings);
-	var that = parseInt(this[0],10),
-		scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
-		scopeVal = scopeTest.height();
-	scopeTest.remove();
-	return (that / scopeVal).toFixed(8) + 'em';
-};
 
-
-$.fn.toPx = function(settings){
-	settings = jQuery.extend({
-		scope: 'body'
-	}, settings);
-	var that = parseFloat(this[0]),
-		scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
-		scopeVal = scopeTest.height();
-	scopeTest.remove();
-	return Math.round(that * scopeVal) + 'px';
-};
+	$.fn.toPx = function(settings){
+		settings = jQuery.extend({
+			scope: 'body'
+		}, settings);
+		var that = parseFloat(this[0]),
+			scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
+			scopeVal = scopeTest.height();
+		scopeTest.remove();
+		return Math.round(that * scopeVal) + 'px';
+	};
+})(jQuery);
