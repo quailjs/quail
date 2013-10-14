8996fd31208f85a1629cc63296fcec413c119b4e
Kevin Miller <kemiller@csumb.edu>
2013-10-13 20:54:57 -0700

    Renamed all tests files to align to test names, fixed examples.

--- test/composite.js
+++ test/composite.js
@@ -53,9 +53,7 @@ var quailTest = {
   },
   
   insertElements : function(callback) {
-    
-      $('body').prepend('<div role="header" id="qunit-wrapper"><h2 id="qunit-banner"></h2><div id="qunit-testrunner-toolbar"></div><h2 id="qunit-userAgent"></h2><ol id="qunit-tests"></ol><div id="qunit-fixture">test markup, will be hidden</div></div>');
-   
+   $('body').prepend('<div role="header" id="qunit-wrapper"><h2 id="qunit-banner"></h2><div id="qunit-testrunner-toolbar"></div><h2 id="qunit-userAgent"></h2><ol id="qunit-tests"></ol><div id="qunit-fixture">test markup, will be hidden</div></div>');
   }
 };
 
