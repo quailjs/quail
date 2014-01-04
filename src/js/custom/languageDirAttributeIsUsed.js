426fe505baac10c7cfcd85a5e65ec1ba623f5fc5
Kevin Miller <kevin@csumb.edu>
2014-01-04 12:20:21 -0800

    Changed to smaller number of elements to search, support ancestor direction.
    #72

--- src/js/custom/languageDirAttributeIsUsed.js
+++ src/js/custom/languageDirAttributeIsUsed.js
@@ -1,19 +1,24 @@
 quail.languageDirAttributeIsUsed = function() {
 	var currentDirection = (quail.html.attr('dir')) ? quail.html.attr('dir').toLowerCase() : 'ltr';
 	var oppositeDirection = (currentDirection === 'ltr') ? 'rtl' : 'ltr';
-	quail.html.find(quail.textSelector).each(function() {
-		currentDirection = ($(this).attr('dir')) ? $(this).attr('dir').toLowerCase() : currentDirection;
+	quail.html.find('p, blockquote, aside, h1, h2, h3, h4, h5, h6').each(function() {
+		if($(this).attr('dir')) {
+			currentDirection = $(this).attr('dir').toLowerCase();
+		}
+		else {
+			currentDirection = ($(this).parent('[dir]').first().attr('dir')) ? $(this).parent('[dir]').first().attr('dir').toLowerCase() : currentDirection;
+		}
 		if(typeof quail.textDirection[currentDirection] === 'undefined') {
 			currentDirection = 'ltr';
 		}
 		oppositeDirection = (currentDirection === 'ltr') ? 'rtl' : 'ltr';
-		var matches = $(this).text().match(quail.textDirection[currentDirection]);
+		var matches = $(this).text().match(quail.textDirection[oppositeDirection]);
 		if(!matches) {
 			return;
 		}
 		matches = matches.length;
 		$(this).find('[dir=' + oppositeDirection + ']').each(function() {
-			var childMatches = $(this).text().match(quail.textDirection[currentDirection]);
+			var childMatches = $(this).text().match(quail.textDirection[oppositeDirection]);
 			if(childMatches) {
 				matches = matches - childMatches.length;
 			}
