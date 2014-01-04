d2c4677ea0f1e9bb815cf2a6481be1ce793398c1
Kevin Miller <kevin@csumb.edu>
2014-01-04 11:52:41 -0800

    Added LTR text direction support to languageDirAttributeIsUsed. #72

--- src/js/core.js
+++ src/js/core.js
@@ -57,9 +57,12 @@ var quail = {
   focusElements : 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, *[tabindex], *[contenteditable]',
 
   /**
-   * Characters that are right-to-left only.
+   * Regex to find right-to-left or left-to-right characters
    */
-  rtlCharacters : /[\u0600-\u06FF]|[\u0750-\u077F]|[\u0590-\u05FF]|[\uFE70-\uFEFF]/mg,
+  textDirection : {
+    rtl : /[\u0600-\u06FF]|[\u0750-\u077F]|[\u0590-\u05FF]|[\uFE70-\uFEFF]/mg,
+    ltr : /[\u0041-\u007A]|[\u00C0-\u02AF]|[\u0388-\u058F]/mg
+  },
 
   /**
    * Main run function for quail. It bundles up some accessibility tests,
--- src/js/custom/languageDirAttributeIsUsed.js
+++ src/js/custom/languageDirAttributeIsUsed.js
@@ -1,12 +1,19 @@
 quail.languageDirAttributeIsUsed = function() {
+	var currentDirection = (quail.html.attr('dir')) ? quail.html.attr('dir').toLowerCase() : 'ltr';
+	var oppositeDirection = (currentDirection === 'ltr') ? 'rtl' : 'ltr';
 	quail.html.find(quail.textSelector).each(function() {
-		var matches = $(this).text().match(quail.rtlCharacters);
+		currentDirection = ($(this).attr('dir')) ? $(this).attr('dir').toLowerCase() : currentDirection;
+		if(typeof quail.textDirection[currentDirection] === 'undefined') {
+			currentDirection = 'ltr';
+		}
+		oppositeDirection = (currentDirection === 'ltr') ? 'rtl' : 'ltr';
+		var matches = $(this).text().match(quail.textDirection[currentDirection]);
 		if(!matches) {
 			return;
 		}
 		matches = matches.length;
-		$(this).find('[dir=rtl]').each(function() {
-			var childMatches = $(this).text().match(quail.rtlCharacters);
+		$(this).find('[dir=' + oppositeDirection + ']').each(function() {
+			var childMatches = $(this).text().match(quail.textDirection[currentDirection]);
 			if(childMatches) {
 				matches = matches - childMatches.length;
 			}
