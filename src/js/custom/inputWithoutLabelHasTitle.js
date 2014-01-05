16baa0a49a79a777fa2045efef59206b0cb06b3f
Kevin Miller <kevin@csumb.edu>
2014-01-05 09:05:49 -0800

    Added test for technique H65, issue #64.

---
+++ src/js/custom/inputWithoutLabelHasTitle.js
@@ -0,0 +1,9 @@
+quail.inputWithoutLabelHasTitle = function() {
+	quail.html.find('input, select, textarea').each(function() {
+		if(!$(this).parent('label').length &&
+			!quail.html.find('label[for=' + $(this).attr('id') + ']').length &&
+			(!$(this).attr('title') || !quail.containsReadableText($(this).attr('title')))) {
+			quail.testFails('inputWithoutLabelHasTitle', $(this));
+		}
+	});
+};
