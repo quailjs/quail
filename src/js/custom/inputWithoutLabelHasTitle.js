1e04711e844cc24ec4e0430fd712388d98a2356e
Kevin Miller <kevin@csumb.edu>
2014-01-06 06:51:34 -0800

    Fixed test for if an input title is unreadable. Travis fail 16420977

--- src/js/custom/inputWithoutLabelHasTitle.js
+++ src/js/custom/inputWithoutLabelHasTitle.js
@@ -2,7 +2,7 @@ quail.inputWithoutLabelHasTitle = function() {
 	quail.html.find('input, select, textarea').each(function() {
 		if(!$(this).parent('label').length &&
 			!quail.html.find('label[for=' + $(this).attr('id') + ']').length &&
-			(!$(this).attr('title') || !quail.containsReadableText($(this).attr('title')))) {
+			(!$(this).attr('title') || quail.isUnreadable($(this).attr('title')))) {
 			quail.testFails('inputWithoutLabelHasTitle', $(this));
 		}
 	});
