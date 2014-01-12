369d427c01bbeaa0a39360f27b6f68356537d61c
Kevin Miller <kevin@csumb.edu>
2014-01-12 13:05:57 -0800

    Added test whiteSpaceInWord per issue #73.

---
+++ src/js/custom/whiteSpaceInWord.js
@@ -0,0 +1,14 @@
+quail.whiteSpaceInWord = function() {
+	var whitespaceGroup, nonWhitespace;
+	quail.html.find(quail.textSelector).each(function() {
+		nonWhitespace = ($(this).text()) ? $(this).text().match(/[^\s\\]/g) : false;
+		whitespaceGroup = ($(this).text()) ? $(this).text().match(/[^\s\\]\s[^\s\\]/g) : false;
+		if(nonWhitespace && 
+				whitespaceGroup &&
+				whitespaceGroup.length > 3 &&
+				whitespaceGroup.length >= (nonWhitespace.length / 2) - 2) {
+					quail.testFails('whiteSpaceInWord', $(this));
+		}
+	});
+};
+
