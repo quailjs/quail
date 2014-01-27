5157d0230c3f041d4d37c278e0d8b54f25c172ce
J. Renée Beach <splendidnoise@gmail.com>
2014-01-26 23:10:48 -0500

    Issue #125: Return code snippet
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/core.js
+++ src/js/core.js
@@ -152,6 +152,8 @@ var quail = {
                      'unknown';
       
       quail.options.testFailed({element  : $element,
+                             selector    : quail.defineUniqueSelector($element.length && $element[0] || null),
+                             location    : window && window.location || null,
                              testName    : testName,
                              test        : quail.accessibilityTests[testName],
                              testability : testability,
@@ -295,5 +297,193 @@ var quail = {
       }
     }
     return false;
+  },
+
+  /**
+   * Creates a page-unique selector for the selected DOM element.
+   *
+   * @param jQuery $element
+   *   An element in a jQuery wrapper.
+   *
+   * @return String
+   *   A unique selector for this element.
+   */
+  defineUniqueSelector: function (element) {
+    /**
+     * Indicates whether the selector string represents a unique DOM element.
+     *
+     * @param String selector
+     *   A string selector that can be used to query a DOM element.
+     *
+     * @return Boolean
+     *   Whether or not the selector string represents a unique DOM element.
+     */
+    function isUniquePath (selector) {
+      return $(selector).length === 1;
+    }
+
+    /**
+     * Creates a selector from the element's id attribute.
+     *
+     * Temporary IDs created by the module that contain "visitorActions" are excluded.
+     *
+     * @param DOM element
+     *
+     * @return String
+     *   An id selector or an empty string.
+     */
+    function applyID (element) {
+      var selector = '';
+      var id = element.id || '';
+      if (id.length > 0) {
+        selector = '#' + id;
+      }
+      return selector;
+    }
+
+    /**
+     * Creates a selector from classes on the element.
+     *
+     * Classes with known functional components like the word 'active' are
+     * excluded because these often denote state, not identity.
+     *
+     * @param DOM element
+     *
+     * @return String
+     *   A selector of classes or an empty string.
+     */
+    function applyClasses (element) {
+      var selector = '';
+      // Try to make a selector from the element's classes.
+      var classes = element.className || '';
+      if (classes.length > 0) {
+        classes = classes.split(/\s+/);
+        // Filter out classes that might represent state.
+        classes = reject(classes, function (cl) {
+          return (/active|enabled|disabled|first|last|only|collapsed|open|clearfix|processed/).test(cl);
+        });
+        if (classes.length > 0) {
+          return '.' + classes.join('.');
+        }
+      }
+      return selector;
+    }
+
+    /**
+     * Finds attributes on the element and creates a selector from them.
+     *
+     * @param DOM element
+     *
+     * @return String
+     *   A selector of attributes or an empty string.
+     */
+    function applyAttributes (element) {
+      var selector = '';
+      var attributes = ['href', 'type'];
+      var value;
+      // Try to make a selector from the element's classes.
+      for (var i = 0, len = attributes.length; i < len; i++) {
+        value = element.attributes[attributes[i]] && element.attributes[attributes[i]].value;
+        if (value) {
+          // If the attr is href and it points to a specific user account,
+          // just tack on the attr name and not the value.
+          if (attributes[i] === 'href' && (/user\/[a-zA-z0-9]+/).test(value)) {
+            selector += '[' + attributes[i] + ']';
+          }
+          else {
+            selector += '[' + attributes[i] + '="' + value + '"]';
+          }
+        }
+      }
+      return selector;
+    }
+
+    /**
+     * Creates a unique selector using id, classes and attributes.
+     *
+     * It is possible that the selector will not be unique if there is no
+     * unique description using only ids, classes and attributes of an
+     * element that exist on the page already. If uniqueness cannot be
+     * determined and is required, you will need to add a unique identifier
+     * to the element through theming development.
+     *
+     * @param DOM element
+     *
+     * @return String
+     *   A unique selector for the element.
+     */
+    function generateSelector (element) {
+      var selector = '';
+      var scopeSelector = '';
+      var pseudoUnique = false;
+      var firstPass = true;
+
+      do {
+        scopeSelector = '';
+        // Try to apply an ID.
+        if ((scopeSelector = applyID(element)).length > 0) {
+          selector = scopeSelector + ' ' + selector;
+          // Assume that a selector with an ID in the string is unique.
+          break;
+        }
+
+        // Try to apply classes.
+        if (!pseudoUnique && (scopeSelector = applyClasses(element)).length > 0) {
+          // If the classes don't create a unique path, tack them on and
+          // continue.
+          selector = scopeSelector + ' ' + selector;
+          // If the classes do create a unique path, mark this selector as
+          // pseudo unique. We will keep attempting to find an ID to really
+          // guarantee uniqueness.
+          if (isUniquePath(selector)) {
+            pseudoUnique = true;
+          }
+        }
+
+        // Process the original element.
+        if (firstPass) {
+          // Try to add attributes.
+          if ((scopeSelector = applyAttributes(element)).length > 0) {
+            // Do not include a space because the attributes qualify the
+            // element. Append classes if they exist.
+            selector = scopeSelector + selector;
+          }
+
+          // Add the element nodeName.
+          selector = element.nodeName.toLowerCase() + selector;
+
+          // The original element has been processed.
+          firstPass = false;
+        }
+
+        // Try the parent element to apply some scope.
+        element = element.parentNode;
+      } while (element && element.nodeType === 1 && element.nodeName !== 'BODY' && element.nodeName !== 'HTML');
+
+      return selector.trim();
+    }
+
+    /**
+     * Helper function to filter items from a list that pass the comparator
+     * test.
+     *
+     * @param array list
+     * @param function comparator
+     *   A function that return a boolean. True means the list item will be
+     *   discarded from the list.
+     * @return array
+     *   A list of items the excludes items that passed the comparator test.
+     */
+    function reject (list, comparator) {
+      var keepers = [];
+      for (var i = 0, il = list.length; i < il; i++) {
+        if (!comparator.call(null, list[i])) {
+          keepers.push(list[i]);
+        }
+      }
+      return keepers;
+    }
+
+    return element && generateSelector(element);
   }
 };
