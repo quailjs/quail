f8393490a4b0b3adc652e3d76424c06993798090
Kevin Miller <kevin@csumb.edu>
2014-02-04 17:17:23 -0800

    Found headersusetomakrsections wasn't trimming text.

--- src/js/custom/headersUseToMarkSections.js
+++ src/js/custom/headersUseToMarkSections.js
@@ -2,7 +2,7 @@ quail.headersUseToMarkSections = function() {
   quail.html.find('p').each(function() {
     var $paragraph = $(this);
     $paragraph.find('strong:first, em:first, i:first, b:first').each(function() {
-      if($paragraph.text() === $(this).text()) {
+      if($paragraph.text().trim() === $(this).text().trim()) {
         quail.testFails('headersUseToMarkSections', $paragraph);
       }
     });
