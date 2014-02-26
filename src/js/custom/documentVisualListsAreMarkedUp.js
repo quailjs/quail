9c99a979976e1eb5c5d7fb7296db9f29c8fca20f
Kevin Miller <kevin@csumb.edu>
2014-02-26 11:12:58 -0800

    Added more symbols to documentVisualListsAreMarkedUp

--- src/js/custom/documentVisualListsAreMarkedUp.js
+++ src/js/custom/documentVisualListsAreMarkedUp.js
@@ -1,10 +1,11 @@
 quail.documentVisualListsAreMarkedUp = function() {
+  var symbols = /(<br(\/)?>)(\s)(♦|›|»|‣|▶|.|◦|✓|◽|•|—|◾|\||\*|&bull;|&#8226;|[0-9].|\(?[0-9]\)|(?:[IXC][MD]|D?C{0,4}))/i;
   quail.html.find(quail.textSelector).each(function() {
     var $element = $(this);
-    $.each(quail.strings.symbols, function(index, item) {
-      if($element.text().split(item).length > 2) {
-        quail.testFails('documentVisualListsAreMarkedUp', $element);
-      }
-    });
+    var matches = $element.html().match(symbols);
+    console.log(matches);
+    if(matches && matches.length > 2) {
+      quail.testFails('documentVisualListsAreMarkedUp', $element);
+    }
   });
 };
