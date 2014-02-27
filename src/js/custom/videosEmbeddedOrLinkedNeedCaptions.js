6e4eadbc6e63ca7c35bf5b8bb1fe731f3a852539
Kevin Miller <kevin@csumb.edu>
2014-02-26 21:18:43 -0800

    Added video track elements to videoEmbeddedOrLinkedNeedCaptions #93.

--- src/js/custom/videosEmbeddedOrLinkedNeedCaptions.js
+++ src/js/custom/videosEmbeddedOrLinkedNeedCaptions.js
@@ -1,14 +1,18 @@
 quail.videosEmbeddedOrLinkedNeedCaptions = function() {
-  quail.html.find('a').each(function() {
-    var $link = $(this);
+  quail.html.find('a, iframe').each(function() {
+    var $video = $(this);
+    var attribute = ($(this).is('iframe')) ? 'src' : 'href';
     $.each(quail.components.video, function(type, callback) {
-      if(callback.isVideo($link.attr('href'))) {
+      if(callback.isVideo($video.attr(attribute))) {
         callback.hasCaptions(function(hasCaptions) {
           if(!hasCaptions) {
-            quail.testFails('videosEmbeddedOrLinkedNeedCaptions', $link);
+            quail.testFails('videosEmbeddedOrLinkedNeedCaptions', $video);
           }
         });
       }
     });
+  });
+  quail.html.find('video:not(video:has(track[kind=subtitles], track[kind=captions]))').each(function() {
+    quail.testFails('videosEmbeddedOrLinkedNeedCaptions', $(this));
   });
 };
