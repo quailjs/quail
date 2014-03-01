49c6b0f4bfa5837b22ddf19c78f10b8a6720b5fb
Kevin Miller <kevin@csumb.edu>
2014-03-01 14:19:16 -0800

    Captured link title attribute, added SCR24 to new window test. Closes #119

--- src/js/custom/aLinksDontOpenNewWindow.js
+++ src/js/custom/aLinksDontOpenNewWindow.js
@@ -2,7 +2,7 @@ quail.aLinksDontOpenNewWindow = function() {
 	quail.html.find('a[target=_new], a[target=_blank], a[target=_blank]').each(function() {
 		var $link = $(this);
 		for(var i = 0; i < quail.strings.newWindow.length; i++) {
-			if($link.text().search(quail.strings.newWindow[i]) > -1) {
+			if(($link.text() + ' ' + $link.attr('title')).search(quail.strings.newWindow[i]) > -1) {
 				return;
 			}
 		}
