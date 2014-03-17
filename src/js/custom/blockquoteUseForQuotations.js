97ef46a4bafa690441580948da16b0febcb218a3
Peter Droogmans <peter@attiks.com>
2014-03-17 21:14:47 +0100

    regexp

--- src/js/custom/blockquoteUseForQuotations.js
+++ src/js/custom/blockquoteUseForQuotations.js
@@ -1,7 +1,7 @@
 quail.blockquoteUseForQuotations = function() {
   quail.html.find('p').each(function() {
-    if ($(this).text().substr(0, 1).search(/[\'\"]/) > -1 &&
-       $(this).text().substr(-1, 1).search(/[\'\"]/) > -1) {
+    if ($(this).text().substr(0, 1).search(/['"]/) > -1 &&
+       $(this).text().substr(-1, 1).search(/['"]/) > -1) {
       quail.testFails('blockquoteUseForQuotations', $(this));
     }
   });
