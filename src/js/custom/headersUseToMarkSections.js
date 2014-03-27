56ecc79f4c41b7dfaf7ff015fa7047ae85c600a4
Kevin Miller <kevin@csumb.edu>
2014-03-26 17:33:06 -0700

    Added test for navigation lacking a header in front of it. #198

--- src/js/custom/headersUseToMarkSections.js
+++ src/js/custom/headersUseToMarkSections.js
@@ -7,4 +7,21 @@ quail.headersUseToMarkSections = function() {
       }
     });
   });
+
+  quail.html.find('ul, ol').each(function() {
+    var $list = $(this);
+    if ($list.prevAll(':header').length ||
+      $list.find('li').length !== $list.find('li:has(a)').length) {
+      return;
+    }
+    var isNavigation = true;
+    $list.find('li:has(a)').each(function() {
+      if ($(this).text().trim() !== $(this).find('a:first').text().trim()) {
+        isNavigation = false;
+      }
+    });
+    if (isNavigation) {
+      quail.testFails('headersUseToMarkSections', $list);
+    }
+  });
 };
