943e2bc977e51a22d6b24c5dc4e58d0a825501df
Kevin Miller <kevin@csumb.edu>
2014-04-01 14:33:38 -0700

    Added notApplicable case to aLinkWithNonText.

--- src/js/custom/aLinkWithNonText.js
+++ src/js/custom/aLinkWithNonText.js
@@ -1,32 +1,38 @@
 quail.aLinkWithNonText = function(quail, test, Case) {
-  test.get('$scope').find('a:has(img, object, embed)').each(function() {
-      var _case = Case({
-        element: this,
-        expected: $(this).closest('.quail-test').data('expected')
+  test.get('$scope').find('a').each(function() {
+    var _case = Case({
+      element: this,
+      expected: $(this).closest('.quail-test').data('expected')
+    });
+    test.add(_case);
+    if(!$(this).is('a:has(img, object, embed)[href]')) {
+      _case.set({
+        'status': 'notApplicable'
       });
-      test.add(_case);
-      if (!quail.isUnreadable($(this).text())) {
-        _case.set({
-          'status': 'passed'
-        });
-        return;
-      }
-      var unreadable = 0;
-      $(this).find('img, object, embed').each(function() {
-        if (($(this).is('img') && quail.isUnreadable($(this).attr('alt'))) ||
-          (!$(this).is('img') && quail.isUnreadable($(this).attr('title')))) {
-          unreadable++;
-        }
+      return;
+    }
+    if (!quail.isUnreadable($(this).text())) {
+      _case.set({
+        'status': 'passed'
       });
-      if ($(this).find('img, object, embed').length === unreadable) {
-        _case.set({
-          'status': 'failed'
-        });
-      }
-      else {
-        _case.set({
-          'status': 'passed'
-        });
+      return;
+    }
+    var unreadable = 0;
+    $(this).find('img, object, embed').each(function() {
+      if (($(this).is('img') && quail.isUnreadable($(this).attr('alt'))) ||
+        (!$(this).is('img') && quail.isUnreadable($(this).attr('title')))) {
+        unreadable++;
       }
     });
+    if ($(this).find('img, object, embed').length === unreadable) {
+      _case.set({
+        'status': 'failed'
+      });
+    }
+    else {
+      _case.set({
+        'status': 'passed'
+      });
+    }
+  });
 };
