9c5d4334187e23b5a17fd6e181213f74fb14e802
Kevin Miller <kevin@csumb.edu>
2014-04-01 14:51:26 -0700

    Converted aSuspiciousLinkText, added nonApplicable case.

--- src/js/custom/aSuspiciousLinkText.js
+++ src/js/custom/aSuspiciousLinkText.js
@@ -1,11 +1,29 @@
-quail.aSuspiciousLinkText = function() {
-  quail.html.find('a').each(function() {
+quail.aSuspiciousLinkText = function(quail, test, Case) {
+  test.get('$scope').find('a').each(function() {
+    var _case = Case({
+      element: this,
+      expected: $(this).closest('.quail-test').data('expected')
+    });
+    test.add(_case);
+    if (!$(this).attr('href')) {
+      _case.set({
+        'status': 'notApplicable'
+      });
+      return;
+    }
     var text = $(this).text();
     $(this).find('img[alt]').each(function() {
       text = text + $(this).attr('alt');
     });
     if (quail.strings.suspiciousLinks.indexOf(quail.cleanString(text)) > -1) {
-      quail.testFails('aSuspiciousLinkText', $(this));
+      _case.set({
+        'status': 'failed'
+      });
+    }
+    else {
+      _case.set({
+        'status': 'passed'
+      });
     }
   });
 };
