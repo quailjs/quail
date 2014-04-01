edd05f50c3b6f3b06f0841adc0f5a716652b98f2
Kevin Miller <kevin@csumb.edu>
2014-04-01 14:19:45 -0700

    Converted test aLinkWithNonText

--- src/js/custom/aLinkWithNonText.js
+++ src/js/custom/aLinkWithNonText.js
@@ -1,17 +1,32 @@
-quail.aLinkWithNonText = function() {
-  quail.html.find('a:has(img, object, embed)').each(function() {
-    if (!quail.isUnreadable($(this).text())) {
-      return;
-    }
-    var unreadable = 0;
-    $(this).find('img, object, embed').each(function() {
-      if (($(this).is('img') && quail.isUnreadable($(this).attr('alt'))) ||
-        (!$(this).is('img') && quail.isUnreadable($(this).attr('title')))) {
-        unreadable++;
+quail.aLinkWithNonText = function(quail, test, Case) {
+  test.get('$scope').find('a:has(img, object, embed)').each(function() {
+      var _case = Case({
+        element: this,
+        expected: $(this).closest('.quail-test').data('expected')
+      });
+      test.add(_case);
+      if (!quail.isUnreadable($(this).text())) {
+        _case.set({
+          'status': 'passed'
+        });
+        return;
+      }
+      var unreadable = 0;
+      $(this).find('img, object, embed').each(function() {
+        if (($(this).is('img') && quail.isUnreadable($(this).attr('alt'))) ||
+          (!$(this).is('img') && quail.isUnreadable($(this).attr('title')))) {
+          unreadable++;
+        }
+      });
+      if ($(this).find('img, object, embed').length === unreadable) {
+        _case.set({
+          'status': 'failed'
+        });
+      }
+      else {
+        _case.set({
+          'status': 'passed'
+        });
       }
     });
-    if ($(this).find('img, object, embed').length === unreadable) {
-      quail.testFails('aLinkWithNonText', $(this));
-    }
-  });
 };
