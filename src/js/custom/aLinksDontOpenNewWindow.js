6eb7cc0f2f510c809f7580cc1798fb0ba7634d99
J. Renée Beach <splendidnoise@gmail.com>
2014-04-01 22:57:42 -0400

    Updated aLinksDontOpenNewWindow test.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/custom/aLinksDontOpenNewWindow.js
+++ src/js/custom/aLinksDontOpenNewWindow.js
@@ -1,11 +1,45 @@
-quail.aLinksDontOpenNewWindow = function() {
-  quail.html.find('a[target=_new], a[target=_blank], a[target=_blank]').each(function() {
+quail.aLinksDontOpenNewWindow = function(quail, test, Case) {
+  // Links without a target attribute pass.
+  test.get('$scope').find('a').not('[target=_new], [target=_blank]').each(function () {
+    test.add(Case({
+      element: this,
+      'expected': $(this).closest('.quail-test').data('expected'),
+      'status': 'passed'
+    }));
+  });
+  // Links with a target attribute pass if the link text indicates that the
+  // link will open a new window.
+  test.get('$scope').find('a[target=_new], a[target=_blank]').each(function() {
     var $link = $(this);
-    for(var i = 0; i < quail.strings.newWindow.length; i++) {
-      if (($link.text() + ' ' + $link.attr('title')).search(quail.strings.newWindow[i]) > -1) {
-        return;
+    var passes = false;
+    var i = 0;
+    var text = $link.text() + ' ' + $link.attr('title');
+    var phrase = '';
+    // Test the link text against strings the indicate the link will open
+    // in a new window.
+    do {
+      phrase = quail.strings.newWindow[i];
+      if (text.search(phrase) > -1) {
+        passes = true;
       }
+      ++i;
+
+    } while (!passes && i < quail.strings.newWindow.length);
+    // Build a Case.
+    if (passes) {
+      debugger;
+      test.add(Case({
+        element: this,
+        'expected': $link.closest('.quail-test').data('expected'),
+        'status': 'passed'
+      }));
     }
-    quail.testFails('aLinksDontOpenNewWindow', $link);
+    else {
+      test.add(Case({
+        element: this,
+        'expected': $link.closest('.quail-test').data('expected'),
+        'status': 'failed'
+      }));
+    }
   });
 };
