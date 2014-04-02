b680ab44e41dfb9b047f74c771a1342f8a8c5693
J. Renée Beach <splendidnoise@gmail.com>
2014-04-01 23:32:57 -0400

    Updated aLinksNotSeparatedBySymbols test.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/custom/aLinksNotSeparatedBySymbols.js
+++ src/js/custom/aLinksNotSeparatedBySymbols.js
@@ -1,8 +1,29 @@
-quail.aLinksNotSeparatedBySymbols = function() {
-  quail.html.find('a').each(function() {
-    if ($(this).next('a').length &&
-      quail.strings.symbols.indexOf($(this).get(0).nextSibling.wholeText.toLowerCase().trim()) !== -1 ) {
-      quail.testFails('aLinksNotSeparatedBySymbols', $(this));
+quail.aLinksNotSeparatedBySymbols = function(quail, test, Case) {
+  test.get('$scope').find('a').each(function() {
+    var $link = $(this);
+    if ($link.next('a').length) {
+      // The string between the links is composed of symbols.
+      if (quail.strings.symbols.indexOf($link.get(0).nextSibling.wholeText.toLowerCase().trim()) !== -1 ) {
+        test.add(Case({
+          element: this,
+          'expected': $link.closest('.quail-test').data('expected'),
+          'status': 'failed'
+        }));
+      }
+      // The string between the links is composed of words.
+      else {
+        test.add(Case({
+          element: this,
+          'expected': $link.closest('.quail-test').data('expected'),
+          'status': 'passed'
+        }));
+      }
+    }
+    // If nothing follows the link, then there is nothing to test.
+    else {
+      test.add(Case({
+        'status': 'notApplicable'
+      }));
     }
   });
 };
