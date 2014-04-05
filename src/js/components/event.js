543b890b190e904dc33f1b1abfd69d163a9150ab
J. Renée Beach <splendidnoise@gmail.com>
2014-04-05 13:48:21 -0400

    Event component tests are now passing.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/components/event.js
+++ src/js/components/event.js
@@ -2,15 +2,26 @@
  * Test callback for tests that look for script events
  *  (like a mouse event has a keyboard event as well).
  */
-quail.components.event = function(testName, options) {
+quail.components.event = function(quail, test, Case, options) {
   var $items = (typeof options.options.selector === 'undefined') ?
-                quail.html.find('*') :
-                quail.html.find(options.options.selector);
+                test.get('$scope').find('*') :
+                test.get('$scope').find(options.options.selector);
   $items.each(function() {
     if (quail.components.hasEventListener($(this), options.options.searchEvent.replace('on', '')) &&
          (typeof options.options.correspondingEvent === 'undefined' ||
          !quail.components.hasEventListener($(this), options.options.correspondingEvent.replace('on', '')))) {
-      quail.testFails(testName, $(this));
+      test.add(Case({
+        element: this,
+        expected: $(this).closest('.quail-test').data('expected'),
+        status: 'failed'
+      }));
+    }
+    else {
+      test.add(Case({
+        element: this,
+        expected: $(this).closest('.quail-test').data('expected'),
+        status: 'passed'
+      }));
     }
   });
 };
