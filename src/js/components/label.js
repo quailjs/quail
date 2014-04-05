42a77a2d2e9942682eabfec94e33fea85a6a5358
J. Renée Beach <splendidnoise@gmail.com>
2014-04-05 15:57:40 -0400

    The label component now works.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/components/label.js
+++ src/js/components/label.js
@@ -1,10 +1,22 @@
-quail.components.label = function(testName, options) {
-  quail.html.find(options.options.selector).each(function() {
+quail.components.label = function(quail, test, Case, options) {
+  var $scope = test.get('$scope');
+  $scope.find(options.options.selector).each(function() {
     if ((!$(this).parent('label').length ||
         !quail.containsReadableText($(this).parent('label'))) &&
-        (!quail.html.find('label[for=' + $(this).attr('id') + ']').length ||
-        !quail.containsReadableText(quail.html.find('label[for=' + $(this).attr('id') + ']')))) {
-      quail.testFails(testName, $(this));
+        (!$scope.find('label[for=' + $(this).attr('id') + ']').length ||
+        !quail.containsReadableText($scope.find('label[for=' + $(this).attr('id') + ']')))) {
+      test.add(Case({
+        element: this,
+        expected: $(this).closest('.quail-test').data('expected'),
+        status: 'failed'
+      }));
+    }
+    else {
+      test.add(Case({
+        element: this,
+        expected: $(this).closest('.quail-test').data('expected'),
+        status: 'passed'
+      }));
     }
   });
 };
