1b7620458aecac12dbbadb56cfde5c6d4155d89c
J. Renée Beach <splendidnoise@gmail.com>
2014-04-05 16:34:26 -0400

    Added a 'passed' status to the placeholder component test.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/components/placeholder.js
+++ src/js/components/placeholder.js
@@ -4,11 +4,11 @@
  */
 quail.components.placeholder = function(quail, test, Case, options) {
 
-  var fails = function (element) {
+  var resolve = function (element, resolution) {
     test.add(Case({
       element: element,
       expected: $(element).closest('.quail-test').data('expected'),
-      status: 'failed'
+      status: resolution
     }));
   };
 
@@ -22,7 +22,7 @@ quail.components.placeholder = function(quail, test, Case, options) {
          ) &&
          !options.options.content
         ) {
-        fails(this);
+        resolve(this, 'failed');
         return;
       }
       else {
@@ -44,22 +44,22 @@ quail.components.placeholder = function(quail, test, Case, options) {
       var regex = /^([0-9]*)(k|kb|mb|k bytes|k byte)$/g;
       var regexResults = regex.exec(text.toLowerCase());
       if (regexResults && regexResults[0].length) {
-        fails(this);
+        resolve(this, 'failed');
       }
+      else if (options.options.empty && quail.isUnreadable(text)) {
+        resolve(this, 'failed');
+      }
+      else if (quail.strings.placeholders.indexOf(text) > -1 ) {
+        resolve(this, 'failed');
+      }
+      // It passes.
       else {
-        if (options.options.empty && quail.isUnreadable(text)) {
-          fails(this);
-        }
-        else {
-          if (quail.strings.placeholders.indexOf(text) > -1 ) {
-            fails(this);
-          }
-        }
+        resolve(this, 'passed');
       }
     }
     else {
       if (options.options.empty && typeof text !== 'number') {
-        fails(this);
+        resolve(this, 'failed');
       }
     }
   });
