32e8c2af99b0085d17319d765bb86834b188f7a4
J. Renée Beach <splendidnoise@gmail.com>
2014-04-05 19:09:12 -0400

    The header component is fixed.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/components/headingLevel.js
+++ src/js/components/headingLevel.js
@@ -1,9 +1,61 @@
-quail.components.headingLevel = function(testName, options) {
+quail.components.headingLevel = function(quail, test, Case, options) {
+
+  /**
+   * This particular test makes it impossible to impose a limit of one
+   * expectation to each case. So we need to deal with multiple expectations.
+   *
+   * Maybe we could make this more generic, but really, a case should only ever
+   * have one expectation, so this is, at the moment, a huge hack.
+   *
+   * @hereDeDragons.
+   */
+  var resolveExpection = function(element) {
+    var expected = $(element).closest('.quail-test').data('expected');
+    var expectations = expected.split('|');
+
+    if (expectations.length > 1 && element.nodeType === 1) {
+      var conditions = {};
+      var condition;
+      // Split apart the compound expectations.
+      for (var i = 0, il = expectations.length; i < il; ++i) {
+        condition = expectations[i].split(':');
+        conditions[condition[0].toUpperCase()] = condition[1];
+      }
+      // Retrieve the expectation for this element.
+      expected = conditions[element.nodeName];
+      // Return nothing if told to ignore or there is no expectation for this
+      // node.
+      if (!expected || expected === 'ignore') {
+        return;
+      }
+    }
+    // Otherwise the expectation is given as a simple value.
+    return expected;
+  };
+
   var priorLevel = false;
-  quail.html.find(':header').each(function() {
+  test.get('$scope').find(':header').each(function() {
     var level = parseInt($(this).get(0).tagName.substr(-1, 1), 10);
+    var element = this;
     if (priorLevel === options.options.headingLevel && level > priorLevel + 1) {
-      quail.testFails(testName, $(this));
+      test.add(Case({
+        element: element,
+        // @todo, make the expected property retrievable through a callback so
+        //   that we don't need to overload a test with this kind of logic.
+        expected: (function (element) {
+          return resolveExpection(element);
+        }(element)),
+        status: 'failed'
+      }));
+    }
+    else {
+      test.add(Case({
+        element: this,
+        expected: (function (element) {
+          return resolveExpection(element);
+        }(element)),
+        status: 'passed'
+      }));
     }
     priorLevel = level;
   });
