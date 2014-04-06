0edf25d9172d0176753b8b9912f7bd24d0baef7b
Kevin Miller <kevin@csumb.edu>
2014-04-06 09:54:55 -0700

    Converted idRefHasCorrespondingId

--- src/js/custom/idRefHasCorrespondingId.js
+++ src/js/custom/idRefHasCorrespondingId.js
@@ -1,7 +1,19 @@
-quail.idRefHasCorrespondingId = function() {
-  quail.html.find('[idref]').each(function() {
-    if (quail.html.find('#' + $(this).attr('idref')).length === 0) {
-      quail.testFails('idRefHasCorrespondingId', $(this));
+quail.idRefHasCorrespondingId = function(quail, test, Case) {
+  test.get('$scope').find('[idref]').each(function() {
+    var _case = Case({
+      element: this,
+      expected: $(this).closest('.quail-test').data('expected')
+    });
+    test.add(_case);
+    if (test.get('$scope').find('#' + $(this).attr('idref')).length === 0) {
+      _case.set({
+        'status': 'failed'
+      });
+    }
+    else {
+      _case.set({
+        'status': 'passed'
+      });
     }
   });
 };
