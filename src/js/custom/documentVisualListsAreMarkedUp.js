bc6b2c2800f60a2c2c9ee642f7826e48a182fabb
Kevin Miller <kevin@csumb.edu>
2014-04-06 08:52:42 -0700

    Converted documentVisualListsAreMarkedUp

--- src/js/custom/documentVisualListsAreMarkedUp.js
+++ src/js/custom/documentVisualListsAreMarkedUp.js
@@ -1,10 +1,16 @@
-quail.documentVisualListsAreMarkedUp = function() {
+quail.documentVisualListsAreMarkedUp = function(quail, test, Case) {
   var symbols = /(<br(\/)?>)(\s)(♦|›|»|‣|▶|.|◦|✓|◽|•|—|◾|\||\*|&bull;|&#8226;|[0-9].|\(?[0-9]\)|[\u25A0-\u25FF]|(?:[IXC][MD]|D?C{0,4}))/i;
-  quail.html.find(quail.textSelector).each(function() {
-    var $element = $(this);
-    var matches = $element.html().match(symbols);
-    if (matches && matches.length > 2) {
-      quail.testFails('documentVisualListsAreMarkedUp', $element);
-    }
+  test.get('$scope').find(quail.textSelector).each(function() {
+    var _case = Case({
+      element: this,
+      expected: $(this).closest('.quail-test').data('expected')
+    });
+    test.add(_case);
+    var matches = $(this).html().match(symbols);
+    _case.set({
+      'status': (matches && matches.length > 2) ?
+        'failed' :
+        'passed'
+    });
   });
 };
