00b3703e9e47fa06612ec326838e35a63ec4f34c
Kevin Miller <kevin@csumb.edu>
2014-04-07 16:59:12 -0700

    Converted tableLayoutDataShouldNotHaveTh

--- src/js/custom/tableLayoutDataShouldNotHaveTh.js
+++ src/js/custom/tableLayoutDataShouldNotHaveTh.js
@@ -1,7 +1,26 @@
-quail.tableLayoutDataShouldNotHaveTh = function() {
-  quail.html.find('table:has(th)').each(function() {
-    if (!quail.isDataTable($(this))) {
-      quail.testFails('tableLayoutDataShouldNotHaveTh', $(this));
+quail.tableLayoutDataShouldNotHaveTh = function (quail, test, Case) {
+  test.get('$scope').find('table').each(function() {
+    var _case = Case({
+      element: this,
+      expected: $(this).closest('.quail-test').data('expected')
+    });
+    test.add(_case);
+    if ($(this).find('th').length !== 0) {
+      if (!quail.isDataTable($(this))) {
+        _case.set({
+          'status': 'failed'
+        });
+      }
+      else {
+        _case.set({
+          'status': 'passed'
+        });
+      }
+    }
+    else {
+      _case.set({
+        'status': 'notApplicable'
+      });
     }
   });
 };
