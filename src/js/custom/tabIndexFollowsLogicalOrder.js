b0e1f64b3a4bf759e2664d8f9c7a705a604bad2b
Kevin Miller <kevin@csumb.edu>
2014-04-07 16:42:45 -0700

    Converted tabIndexFollowsLogicalOrder
    Still needs split expectations

--- src/js/custom/tabIndexFollowsLogicalOrder.js
+++ src/js/custom/tabIndexFollowsLogicalOrder.js
@@ -1,9 +1,21 @@
-quail.tabIndexFollowsLogicalOrder = function() {
+quail.tabIndexFollowsLogicalOrder = function (quail, test, Case) {
   var index = 0;
-  quail.html.find('[tabindex]').each(function() {
+  test.get('$scope').find('[tabindex]').each(function() {
+    var _case = Case({
+      element: this,
+      expected: $(this).closest('.quail-test').data('expected')
+    });
+    test.add(_case);
     if (parseInt($(this).attr('tabindex'), 10) >= 0 &&
         parseInt($(this).attr('tabindex'), 10) !== index + 1) {
-      quail.testFails('tabIndexFollowsLogicalOrder', $(this));
+      _case.set({
+        'status': 'failed'
+      });
+    }
+    else {
+      _case.set({
+        'status': 'passed'
+      });
     }
     index++;
   });
