e467ebb5ad2151a818b8d6da2a166b86e416ae12
Kevin Miller <kevin@csumb.edu>
2014-04-07 09:06:09 -0700

    Converted imgWithMathShouldHaveMathEquivalent

--- src/js/custom/imgWithMathShouldHaveMathEquivalent.js
+++ src/js/custom/imgWithMathShouldHaveMathEquivalent.js
@@ -1,7 +1,14 @@
-quail.imgWithMathShouldHaveMathEquivalent = function() {
-  quail.html.find('img:not(img:has(math), img:has(tagName))').each(function() {
+quail.imgWithMathShouldHaveMathEquivalent = function (quail, test, Case) {
+  test.get('$scope').find('img:not(img:has(math), img:has(tagName))').each(function() {
+    var _case = Case({
+      element: this,
+      expected: $(this).closest('.quail-test').data('expected')
+    });
+    test.add(_case);
     if (!$(this).parent().find('math').length) {
-      quail.testFails('imgWithMathShouldHaveMathEquivalent', $(this));
+      _case.set({
+        'status': 'failed'
+      });
     }
   });
 };
