7bf31dede6e4ccc369d638792650e78e72fd079c
J. Renée Beach <splendidnoise@gmail.com>
2014-04-06 23:18:40 -0400

    Refined label.js.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/components/label.js
+++ src/js/components/label.js
@@ -1,22 +1,25 @@
 quail.components.label = function(quail, test, Case, options) {
   var $scope = test.get('$scope');
-  $scope.find(options.options.selector).each(function() {
-    if ((!$(this).parent('label').length ||
-        !quail.containsReadableText($(this).parent('label'))) &&
-        (!$scope.find('label[for=' + $(this).attr('id') + ']').length ||
-        !quail.containsReadableText($scope.find('label[for=' + $(this).attr('id') + ']')))) {
-      test.add(Case({
-        element: this,
-        expected: $(this).closest('.quail-test').data('expected'),
-        status: 'failed'
-      }));
-    }
-    else {
-      test.add(Case({
-        element: this,
-        expected: $(this).closest('.quail-test').data('expected'),
-        status: 'passed'
-      }));
-    }
+  $scope.each(function() {
+    var $local = $(this);
+    $local.find(options.options.selector).each(function() {
+      if ((!$(this).parent('label').length ||
+          !quail.containsReadableText($(this).parent('label'))) &&
+          (!$local.find('label[for=' + $(this).attr('id') + ']').length ||
+          !quail.containsReadableText($local.find('label[for=' + $(this).attr('id') + ']')))) {
+        test.add(Case({
+          element: this,
+          expected: $(this).closest('.quail-test').data('expected'),
+          status: 'failed'
+        }));
+      }
+      else {
+        test.add(Case({
+          element: this,
+          expected: $(this).closest('.quail-test').data('expected'),
+          status: 'passed'
+        }));
+      }
+    });
   });
 };
