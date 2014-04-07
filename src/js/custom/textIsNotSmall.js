ff6e28c3c70351a9015a2f5cbadd5f2f1154b794
Peter Droogmans <peter@attiks.com>
2014-04-07 21:03:32 +0200

    textIsNotSmall converted

--- src/js/custom/textIsNotSmall.js
+++ src/js/custom/textIsNotSmall.js
@@ -1,12 +1,28 @@
-quail.textIsNotSmall = function() {
-  quail.html.find(quail.textSelector).each(function() {
+quail.textIsNotSmall = function(quail, test, Case) {
+  test.get('$scope').find(quail.textSelector).each(function() {
     var fontSize = $(this).css('font-size');
     if (fontSize.search('em') > 0) {
       fontSize = quail.components.convertToPx(fontSize);
     }
     fontSize = parseInt(fontSize.replace('px', ''), 10);
+
     if (fontSize < 10) {
-      quail.testFails('textIsNotSmall', $(this));
+      test.add(Case({
+        element: this,
+        expected: (function (element) {
+          return quail.components.resolveExpectation(element);
+        }(this)),
+        status: 'failed'
+      }));
+    }
+    else {
+      test.add(Case({
+        element: this,
+        expected: (function (element) {
+          return quail.components.resolveExpectation(element);
+        }(this)),
+        status: 'passed'
+      }));
     }
   });
 };
