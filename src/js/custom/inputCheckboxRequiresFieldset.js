146c5290cd0d0e0c80c9fc9d985f5342dc27ec20
Kevin Miller <kevin@csumb.edu>
2014-04-07 09:05:58 -0700

    Converted inputCheckboxRequiresFieldset
    imgWithMathShouldHaveMathEquivalent,

--- src/js/custom/inputCheckboxRequiresFieldset.js
+++ src/js/custom/inputCheckboxRequiresFieldset.js
@@ -1,7 +1,19 @@
-quail.inputCheckboxRequiresFieldset = function() {
-  quail.html.find(':checkbox').each(function() {
+quail.inputCheckboxRequiresFieldset = function (quail, test, Case) {
+  test.get('$scope').find(':checkbox').each(function() {
+    var _case = Case({
+      element: this,
+      expected: $(this).closest('.quail-test').data('expected')
+    });
+    test.add(_case);
     if (!$(this).parents('fieldset').length) {
-      quail.testFails('inputCheckboxRequiresFieldset', $(this));
+      _case.set({
+        'status': 'failed'
+      });
+    }
+    else {
+      _case.set({
+        'status': 'passed'
+      });
     }
   });
 };
