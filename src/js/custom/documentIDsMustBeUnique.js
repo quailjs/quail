f48d596d36add846500c63b458851777cc98cb51
J. Renée Beach <splendidnoise@gmail.com>
2014-04-06 23:39:57 -0400

    Converted documentIDsMustBeUnique.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/custom/documentIDsMustBeUnique.js
+++ src/js/custom/documentIDsMustBeUnique.js
@@ -1,4 +1,38 @@
 quail.documentIDsMustBeUnique = function(quail, test, Case) {
+
+  /**
+   * This particular test makes it impossible to impose a limit of one
+   * expectation to each case. So we need to deal with multiple expectations.
+   *
+   * Maybe we could make this more generic, but really, a case should only ever
+   * have one expectation, so this is, at the moment, a huge hack.
+   *
+   * @hereDeDragons.
+   */
+  var resolveExpection = function(element) {
+    var expected = $(element).closest('.quail-test').data('expected');
+    var expectations = expected.split('|');
+
+    if (expectations.length > 1 && element.nodeType === 1) {
+      var conditions = {};
+      var condition;
+      // Split apart the compound expectations.
+      for (var i = 0, il = expectations.length; i < il; ++i) {
+        condition = expectations[i].split(':');
+        conditions[condition[0].toUpperCase()] = condition[1];
+      }
+      // Retrieve the expectation for this element.
+      expected = conditions[element.nodeName];
+      // Return nothing if told to ignore or there is no expectation for this
+      // node.
+      if (!expected || expected === 'ignore') {
+        return;
+      }
+    }
+    // Otherwise the expectation is given as a simple value.
+    return expected;
+  };
+
   var ids = {};
   test.get('$scope').find(':not([id])').each(function() {
     var _case = Case({
@@ -13,7 +47,9 @@ quail.documentIDsMustBeUnique = function(quail, test, Case) {
   test.get('$scope').find('[id]').each(function() {
     var _case = Case({
       element: this,
-      expected: $(this).closest('.quail-test').data('expected')
+      expected: (function (element) {
+        return resolveExpection(element);
+      }(this))
     });
     test.add(_case);
     if (typeof ids[$(this).attr('id')] === 'undefined') {
