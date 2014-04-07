53c2b49f745f5ba9136f8144f81ffc835f7b7465
Peter Droogmans <peter@attiks.com>
2014-04-07 20:23:03 +0200

    whiteSpaceInWord converted

--- src/js/custom/whiteSpaceInWord.js
+++ src/js/custom/whiteSpaceInWord.js
@@ -1,13 +1,28 @@
-quail.whiteSpaceInWord = function() {
+quail.whiteSpaceInWord = function(quail, test, Case) {
   var whitespaceGroup, nonWhitespace;
-  quail.html.find(quail.textSelector).each(function() {
+  test.get('$scope').find(quail.textSelector).each(function() {
     nonWhitespace = ($(this).text()) ? $(this).text().match(/[^\s\\]/g) : false;
     whitespaceGroup = ($(this).text()) ? $(this).text().match(/[^\s\\]\s[^\s\\]/g) : false;
     if (nonWhitespace &&
         whitespaceGroup &&
-      whitespaceGroup.length > 3 &&
-      whitespaceGroup.length >= (nonWhitespace.length / 2) - 2) {
-      quail.testFails('whiteSpaceInWord', $(this));
+        whitespaceGroup.length > 3 &&
+        whitespaceGroup.length >= (nonWhitespace.length / 2) - 2) {
+      test.add(Case({
+        element: this,
+        expected: (function (element) {
+          return quail.components.resolveExpectation(element);
+        }(this)),
+        status: 'failed'
+      }));
+    }
+    else {
+      test.add(Case({
+        element: this,
+        expected: (function (element) {
+          return quail.components.resolveExpectation(element);
+        }(this)),
+        status: 'passed'
+      }));
     }
   });
 };
