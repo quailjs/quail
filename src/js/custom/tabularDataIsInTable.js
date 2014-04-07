0e25747f53d9a07bbc78c092653961532ffcb540
Peter Droogmans <peter@attiks.com>
2014-04-07 22:29:53 +0200

    tabularDataIsInTable converted

--- src/js/custom/tabularDataIsInTable.js
+++ src/js/custom/tabularDataIsInTable.js
@@ -1,7 +1,22 @@
-quail.tabularDataIsInTable = function() {
-  quail.html.find('pre').each(function() {
+quail.tabularDataIsInTable = function(quail, test, Case) {
+  test.get('$scope').find('pre').each(function() {
     if ($(this).html().search('\t') >= 0) {
-      quail.testFails('tabularDataIsInTable', $(this));
+      test.add(Case({
+        element: this,
+        expected: (function (element) {
+          return quail.components.resolveExpectation(element);
+        }(this)),
+        status: 'failed'
+      }));
+    }
+    else {
+      test.add(Case({
+        element: this,
+        expected: (function (element) {
+          return quail.components.resolveExpectation(element);
+        }(this)),
+        status: 'passed'
+      }));
     }
   });
 };
