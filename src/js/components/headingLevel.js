d4f4961efe64f302590e68041f9f9d3bebfbc4de
J. Renée Beach <splendidnoise@gmail.com>
2014-04-07 00:08:01 -0400

    Created a component for resolving multiple case expectations in a single test scenario.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/components/headingLevel.js
+++ src/js/components/headingLevel.js
@@ -1,38 +1,4 @@
 quail.components.headingLevel = function(quail, test, Case, options) {
-
-  /**
-   * This particular test makes it impossible to impose a limit of one
-   * expectation to each case. So we need to deal with multiple expectations.
-   *
-   * Maybe we could make this more generic, but really, a case should only ever
-   * have one expectation, so this is, at the moment, a huge hack.
-   *
-   * @hereDeDragons.
-   */
-  var resolveExpection = function(element) {
-    var expected = $(element).closest('.quail-test').data('expected');
-    var expectations = expected.split('|');
-
-    if (expectations.length > 1 && element.nodeType === 1) {
-      var conditions = {};
-      var condition;
-      // Split apart the compound expectations.
-      for (var i = 0, il = expectations.length; i < il; ++i) {
-        condition = expectations[i].split(':');
-        conditions[condition[0].toUpperCase()] = condition[1];
-      }
-      // Retrieve the expectation for this element.
-      expected = conditions[element.nodeName];
-      // Return nothing if told to ignore or there is no expectation for this
-      // node.
-      if (!expected || expected === 'ignore') {
-        return;
-      }
-    }
-    // Otherwise the expectation is given as a simple value.
-    return expected;
-  };
-
   var priorLevel = false;
   test.get('$scope').find(':header').each(function() {
     var level = parseInt($(this).get(0).tagName.substr(-1, 1), 10);
@@ -43,7 +9,7 @@ quail.components.headingLevel = function(quail, test, Case, options) {
         // @todo, make the expected property retrievable through a callback so
         //   that we don't need to overload a test with this kind of logic.
         expected: (function (element) {
-          return resolveExpection(element);
+          return quail.components.resolveExpectation(element);
         }(element)),
         status: 'failed'
       }));
@@ -52,7 +18,7 @@ quail.components.headingLevel = function(quail, test, Case, options) {
       test.add(Case({
         element: this,
         expected: (function (element) {
-          return resolveExpection(element);
+          return quail.components.resolveExpectation(element);
         }(element)),
         status: 'passed'
       }));
--- src/js/custom/documentIDsMustBeUnique.js
+++ src/js/custom/documentIDsMustBeUnique.js
@@ -1,38 +1,4 @@
 quail.documentIDsMustBeUnique = function(quail, test, Case) {
-
-  /**
-   * This particular test makes it impossible to impose a limit of one
-   * expectation to each case. So we need to deal with multiple expectations.
-   *
-   * Maybe we could make this more generic, but really, a case should only ever
-   * have one expectation, so this is, at the moment, a huge hack.
-   *
-   * @hereDeDragons.
-   */
-  var resolveExpection = function(element) {
-    var expected = $(element).closest('.quail-test').data('expected');
-    var expectations = expected.split('|');
-
-    if (expectations.length > 1 && element.nodeType === 1) {
-      var conditions = {};
-      var condition;
-      // Split apart the compound expectations.
-      for (var i = 0, il = expectations.length; i < il; ++i) {
-        condition = expectations[i].split(':');
-        conditions[condition[0].toUpperCase()] = condition[1];
-      }
-      // Retrieve the expectation for this element.
-      expected = conditions[element.nodeName];
-      // Return nothing if told to ignore or there is no expectation for this
-      // node.
-      if (!expected || expected === 'ignore') {
-        return;
-      }
-    }
-    // Otherwise the expectation is given as a simple value.
-    return expected;
-  };
-
   var ids = {};
   test.get('$scope').find(':not([id])').each(function() {
     var _case = Case({
@@ -48,7 +14,7 @@ quail.documentIDsMustBeUnique = function(quail, test, Case) {
     var _case = Case({
       element: this,
       expected: (function (element) {
-        return resolveExpection(element);
+        return quail.components.resolveExpectation(element);
       }(this))
     });
     test.add(_case);
