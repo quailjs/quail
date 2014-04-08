b5a4236e2cc850dcc9b0f1d46ff1145caef59bdb
Peter Droogmans <peter@attiks.com>
2014-04-08 22:32:14 +0200

    videosEmbeddedOrLinkedNeedCaptions converted

--- src/js/custom/videosEmbeddedOrLinkedNeedCaptions.js
+++ src/js/custom/videosEmbeddedOrLinkedNeedCaptions.js
@@ -1,20 +1,23 @@
 quail.videosEmbeddedOrLinkedNeedCaptions = function (quail, test, Case) {
 
   quail.components.video.findVideos(test.get('$scope'), function(element, pass) {
-    var _case = Case({
-      element: element.get(0),
-      expected: element.closest('.quail-test').data('expected')
-    });
-    test.add(_case);
     if (!pass) {
-      _case.set({
-        'status': 'failed'
-      });
+      test.add(Case({
+        element: this,
+        expected: (function (element) {
+          return quail.components.resolveExpectation(element);
+        }(this)),
+        status: 'failed'
+      }));
     }
     else {
-      _case.set({
-        'status': 'passed'
-      });
+      test.add(Case({
+        element: this,
+        expected: (function (element) {
+          return quail.components.resolveExpectation(element);
+        }(this)),
+        status: 'passed'
+      }));
     }
   });
 
