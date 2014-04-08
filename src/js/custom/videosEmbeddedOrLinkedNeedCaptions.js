10bece44b0cb4e214ddfb7c0891bdc4e068fe36c
Kevin Miller <kevin@csumb.edu>
2014-04-07 21:57:33 -0700

    Converted videosEmbeddedOrLinkedNeedCaptions

--- src/js/custom/videosEmbeddedOrLinkedNeedCaptions.js
+++ src/js/custom/videosEmbeddedOrLinkedNeedCaptions.js
@@ -1,8 +1,20 @@
-quail.videosEmbeddedOrLinkedNeedCaptions = function() {
+quail.videosEmbeddedOrLinkedNeedCaptions = function (quail, test, Case) {
 
-  quail.components.video.findVideos(quail.html, function(element, pass) {
+  quail.components.video.findVideos(test.get('$scope'), function(element, pass) {
+    var _case = Case({
+      element: element.get(0),
+      expected: element.closest('.quail-test').data('expected')
+    });
+    test.add(_case);
     if (!pass) {
-      quail.testFails('videosEmbeddedOrLinkedNeedCaptions', element);
+      _case.set({
+        'status': 'failed'
+      });
+    }
+    else {
+      _case.set({
+        'status': 'passed'
+      });
     }
   });
 
