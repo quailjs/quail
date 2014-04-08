1dc62e4f8373247683759f27c6aee934e063e32b
Kevin Miller <kevin@csumb.edu>
2014-04-08 07:15:56 -0700

    Converted skipToContentLinkProvided

--- src/js/custom/skipToContentLinkProvided.js
+++ src/js/custom/skipToContentLinkProvided.js
@@ -1,20 +1,32 @@
-quail.skipToContentLinkProvided = function() {
+/**globals console:true */
+quail.skipToContentLinkProvided = function(quail, test, Case) {
   var skipLinkFound = false;
-  quail.html.find('a[href*="#"]').each(function() {
+  var _case = Case({
+    element: test.get('$scope').get(0),
+    expected: test.get('$scope').data('expected')
+  });
+  test.add(_case);
+  test.get('$scope').find('a[href*="#"]').each(function() {
     var $link = $(this);
     $.each(quail.strings.skipContent, function() {
       if ($link.text().search(this) > -1 &&
-          quail.html.find('#' + $link.attr('href').split('#').pop()).length
+          test.get('$scope').find('#' + $link.attr('href').split('#').pop()).length
           ) {
         $link.focus();
         if ($link.is(':visible') && $link.css('visibility') !== 'hidden') {
           skipLinkFound = true;
+          _case.set({
+            'status': 'passed'
+          });
+          return;
         }
         $link.blur();
       }
     });
   });
   if (!skipLinkFound) {
-    quail.testFails('skipToContentLinkProvided', quail.html);
+    _case.set({
+      'status': 'failed'
+    });
   }
 };
