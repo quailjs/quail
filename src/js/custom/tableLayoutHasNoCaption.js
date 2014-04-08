06fac76afe6c537b1a77a635b307fff75d4dad7d
Kevin Miller <kevin@csumb.edu>
2014-04-07 21:34:01 -0700

    Converted tableLayoutHasNoCaption

--- src/js/custom/tableLayoutHasNoCaption.js
+++ src/js/custom/tableLayoutHasNoCaption.js
@@ -1,7 +1,26 @@
-quail.tableLayoutHasNoCaption = function() {
-  quail.html.find('table:has(caption)').each(function() {
-    if (!quail.isDataTable($(this))) {
-      quail.testFails('tableLayoutHasNoCaption', $(this));
+quail.tableLayoutHasNoCaption = function (quail, test, Case) {
+  test.get('$scope').find('table').each(function() {
+    var _case = Case({
+      element: this,
+      expected: $(this).closest('.quail-test').data('expected')
+    });
+    test.add(_case);
+    if ($(this).find('caption').length) {
+      if (!quail.isDataTable($(this))) {
+        _case.set({
+          'status': 'failed'
+        });
+      }
+      else {
+        _case.set({
+          'status': 'passed'
+        });
+      }
+    }
+    else {
+      _case.set({
+        'status': 'notApplicable'
+      });
     }
   });
 };
