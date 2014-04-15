301c7172698e2ec78afcad7e2540f2c99ede6b22
J. Renée Beach <splendidnoise@gmail.com>
2014-04-15 02:06:54 -0400

    Updated tableLayoutHasNoSummary.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/custom/tableLayoutHasNoSummary.js
+++ src/js/custom/tableLayoutHasNoSummary.js
@@ -1,7 +1,17 @@
-quail.tableLayoutHasNoSummary = function() {
-  quail.html.find('table[summary]').each(function() {
-    if (!quail.isDataTable($(this)) && !quail.isUnreadable($(this).attr('summary'))) {
-      quail.testFails('tableLayoutHasNoSummary', $(this));
-    }
+quail.tableLayoutHasNoSummary = function(quail, test, Case) {
+  test.get('$scope').each(function () {
+    var $local = $(this);
+    $local.find('table[summary]').each(function() {
+      var _case = test.add(Case({
+        element: this,
+        expected: $(this).closest('.quail-test').data('expected')
+      }));
+      if (!quail.isDataTable($(this)) && !quail.isUnreadable($(this).attr('summary'))) {
+        _case.set({status: 'failed'});
+      }
+      else {
+        _case.set({status: 'passed'});
+      }
+    });
   });
 };
