807734d1165c0ced935c396fa0b1c4d578ec7938
J. Renée Beach <splendidnoise@gmail.com>
2014-04-14 23:27:47 -0400

    Updated event.js
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/components/event.js
+++ src/js/components/event.js
@@ -3,25 +3,26 @@
  *  (like a mouse event has a keyboard event as well).
  */
 quail.components.event = function(quail, test, Case, options) {
-  var $items = (typeof options.options.selector === 'undefined') ?
-                test.get('$scope').find('*') :
-                test.get('$scope').find(options.options.selector);
+  // Ugly.
+  options = options && options.options || {};
+  var $scope = test.get('$scope');
+  var $items = options.selector && $scope.find(options.selector) || $scope.find('*');
+  var searchEvent = options.searchEvent || '';
+  var correspondingEvent = options.correspondingEvent || '';
   $items.each(function() {
-    if (quail.components.hasEventListener($(this), options.options.searchEvent.replace('on', '')) &&
-         (typeof options.options.correspondingEvent === 'undefined' ||
-         !quail.components.hasEventListener($(this), options.options.correspondingEvent.replace('on', '')))) {
-      test.add(Case({
-        element: this,
-        expected: $(this).closest('.quail-test').data('expected'),
-        status: 'failed'
-      }));
+    var hasOnListener = quail.components.hasEventListener($(this), searchEvent.replace('on', ''));
+    var hasCorrespondingEvent = !!correspondingEvent.length;
+    var hasSpecificCorrespondingEvent = quail.components.hasEventListener($(this), correspondingEvent.replace('on', ''));
+    var expected = $(this).closest('.quail-test').data('expected');
+    var _case = test.add(Case({
+      element: this,
+      expected: expected
+    }));
+    if (hasOnListener && (!hasCorrespondingEvent || !hasSpecificCorrespondingEvent)) {
+      _case.set({status: 'failed'});
     }
     else {
-      test.add(Case({
-        element: this,
-        expected: $(this).closest('.quail-test').data('expected'),
-        status: 'passed'
-      }));
+      _case.set({status: 'passed'});
     }
   });
 };
--- src/js/components/hasEventListener.js
+++ src/js/components/hasEventListener.js
@@ -13,7 +13,7 @@ quail.components.hasEventListener = function(element, event) {
   // Certain elements always have default events, so we create a new element to compare default events.
   if ($(element).is('a[href], input, button, video, textarea') &&
     typeof $(element)[0][event] !== 'undefined' &&
-    event === 'click') {
+    (event === 'click' || event === 'focus')) {
     if ($(element)[0][event].toString().search(/^\s*function\s*(\b[a-z$_][a-z0-9$_]*\b)*\s*\((|([a-z$_][a-z0-9$_]*)(\s*,[a-z$_][a-z0-9$_]*)*)\)\s*{\s*\[native code\]\s*}\s*$/i) > -1) {
       return false;
     }
