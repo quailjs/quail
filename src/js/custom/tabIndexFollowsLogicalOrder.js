dd428464096521a86aeb1e678e66fe16b4e36206
J. Renée Beach <splendidnoise@gmail.com>
2014-04-15 01:56:15 -0400

    Updated tabIndexFollowsLogicalOrder.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/custom/tabIndexFollowsLogicalOrder.js
+++ src/js/custom/tabIndexFollowsLogicalOrder.js
@@ -1,22 +1,29 @@
 quail.tabIndexFollowsLogicalOrder = function (quail, test, Case) {
-  var index = 0;
-  test.get('$scope').find('[tabindex]').each(function() {
-    var _case = Case({
-      element: this,
-      expected: $(this).closest('.quail-test').data('expected')
+  test.get('$scope').each(function () {
+    var $local = $(this);
+    var index = 0;
+    $local.find('[tabindex]').each(function() {
+      var $el = $(this);
+      var tabindex = $el.attr('tabindex');
+      if (parseInt(tabindex, 10) >= 0 && parseInt(tabindex, 10) !== index + 1) {
+        test.add(Case({
+          element: this,
+          expected: (function (element) {
+            return quail.components.resolveExpectation(element);
+          }(this)),
+          status: 'failed'
+        }));
+      }
+      else {
+        test.add(Case({
+          element: this,
+          expected: (function (element) {
+            return quail.components.resolveExpectation(element);
+          }(this)),
+          status: 'passed'
+        }));
+      }
+      index++;
     });
-    test.add(_case);
-    if (parseInt($(this).attr('tabindex'), 10) >= 0 &&
-        parseInt($(this).attr('tabindex'), 10) !== index + 1) {
-      _case.set({
-        'status': 'failed'
-      });
-    }
-    else {
-      _case.set({
-        'status': 'passed'
-      });
-    }
-    index++;
   });
 };
