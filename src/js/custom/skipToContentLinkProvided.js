f65242b729550b4a47dc696eff0b21b3e444ccfb
J. Renée Beach <splendidnoise@gmail.com>
2014-04-15 01:37:25 -0400

    Updated skipToContentLinkProvided.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- src/js/custom/skipToContentLinkProvided.js
+++ src/js/custom/skipToContentLinkProvided.js
@@ -1,32 +1,40 @@
 /**globals console:true */
 quail.skipToContentLinkProvided = function(quail, test, Case) {
-  var skipLinkFound = false;
-  var _case = Case({
-    element: test.get('$scope').get(0),
-    expected: test.get('$scope').data('expected')
-  });
-  test.add(_case);
-  test.get('$scope').find('a[href*="#"]').each(function() {
-    var $link = $(this);
-    $.each(quail.strings.skipContent, function() {
-      if ($link.text().search(this) > -1 &&
-          test.get('$scope').find('#' + $link.attr('href').split('#').pop()).length
-          ) {
-        $link.focus();
-        if ($link.is(':visible') && $link.css('visibility') !== 'hidden') {
-          skipLinkFound = true;
-          _case.set({
-            'status': 'passed'
-          });
-          return;
+  test.get('$scope').each(function () {
+    var $local = $(this);
+    var skipLinkFound = false;
+
+    $local.find('a[href*="#"]').each(function() {
+      if (skipLinkFound) {
+        return;
+      }
+      var $link = $(this);
+
+      var fragment = $link.attr('href').split('#').pop();
+      var $target = $local.find('#' + fragment);
+      var strs = quail.strings.skipContent.slice();
+      while (!skipLinkFound && strs.length) {
+        var str = strs.pop();
+        if ($link.text().search(str) > -1 && $target.length) {
+          $link.focus();
+          if ($link.is(':visible') && $link.css('visibility') !== 'hidden') {
+            skipLinkFound = true;
+            test.add(Case({
+              element: $link.get(0),
+              expected: $link.closest('.quail-test').data('expected'),
+              'status': 'passed'
+            }));
+            return;
+          }
+          $link.blur();
         }
-        $link.blur();
       }
     });
+    if (!skipLinkFound) {
+      test.add(Case({
+        expected: $local.data('expected') || $local.find('[data-expected]').data('expected'),
+        'status': 'failed'
+      }));
+    }
   });
-  if (!skipLinkFound) {
-    _case.set({
-      'status': 'failed'
-    });
-  }
 };
