c66ea4b217ff5ab6b48d3a042c5212b7f8b5ba35
Kevin Miller <kevin@csumb.edu>
2014-04-21 15:09:18 -0700

    Fixed elements for videosEmbedded test.

--- src/js/custom/videosEmbeddedOrLinkedNeedCaptions.js
+++ src/js/custom/videosEmbeddedOrLinkedNeedCaptions.js
@@ -3,22 +3,21 @@ quail.videosEmbeddedOrLinkedNeedCaptions = function (quail, test, Case) {
   quail.components.video.findVideos(test.get('$scope'), function(element, pass) {
     if (!pass) {
       test.add(Case({
-        element: this,
+        element: element,
         expected: (function (element) {
           return quail.components.resolveExpectation(element);
-        }(this)),
+        }(element)),
         status: 'failed'
       }));
     }
     else {
       test.add(Case({
-        element: this,
+        element: element,
         expected: (function (element) {
           return quail.components.resolveExpectation(element);
-        }(this)),
+        }(element)),
         status: 'passed'
       }));
     }
   });
-
 };
