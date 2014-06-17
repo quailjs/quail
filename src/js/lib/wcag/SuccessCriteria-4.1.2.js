ac99d262c4c861fb89cb08154891c6b3c2e712ff
Peter Droogmans <peter@attiks.com>
2014-06-17 23:05:24 +0200

    failing test added

--- src/js/lib/wcag/SuccessCriteria-4.1.2.js
+++ src/js/lib/wcag/SuccessCriteria-4.1.2.js
@@ -26,53 +26,66 @@ quail.guidelines.wcag.successCriteria['4.1.2'] = (function (quail) {
 
       // Cycle through the cases in the Success Criteria.
       sc.each(function (index, _case) {
-        var selector = _case.get('selector');
         var conclusion = 'untested';
 
-        if (selector) {
+        if (_case.get('status') !== 'notTested') {
+          var selector = _case.get('selector');
+          if (selector) {
 
-          // @dev, we'll look at each test individually for this selector.
+            // @dev, we'll look at each test individually for this selector.
 
-          // Process 'labelsAreAssignedToAnInput'.
-          var cases_labelsAreAssignedToAnInput = labelsAreAssignedToAnInput.findCasesBySelector(selector);
-          // Process 'labelMustBeUnique'.
-          var cases_labelMustBeUnique = labelMustBeUnique.findCasesBySelector(selector);
-          // Process 'inputWithoutLabelHasTitle'.
-          var cases_inputWithoutLabelHasTitle = inputWithoutLabelHasTitle.findCasesBySelector(selector);
+            // Process 'labelsAreAssignedToAnInput'.
+            var cases_labelsAreAssignedToAnInput = labelsAreAssignedToAnInput.findCasesBySelector(selector);
+            // Process 'labelMustBeUnique'.
+            var cases_labelMustBeUnique = labelMustBeUnique.findCasesBySelector(selector);
+            // Process 'inputWithoutLabelHasTitle'.
+            var cases_inputWithoutLabelHasTitle = inputWithoutLabelHasTitle.findCasesBySelector(selector);
 
-          var passing = ['passed', 'notApplicable'];
+            var passing = ['passed', 'notApplicable'];
 
-          // Make sure the arrays are not empty.
-          if ((cases_labelsAreAssignedToAnInput.length === 0 || cases_labelsAreAssignedToAnInput[0].hasStatus(passing)) &&
-            (cases_labelMustBeUnique.length === 0 || cases_labelMustBeUnique[0].hasStatus(passing)) &&
-            (cases_inputWithoutLabelHasTitle.length === 0 || cases_inputWithoutLabelHasTitle[0].hasStatus(passing))) {
-            conclusion = 'passed';
+            // Make sure the arrays are not empty.
+            if ((cases_labelsAreAssignedToAnInput.length === 0 || cases_labelsAreAssignedToAnInput[0].hasStatus(passing)) &&
+              (cases_labelMustBeUnique.length === 0 || cases_labelMustBeUnique[0].hasStatus(passing)) &&
+              (cases_inputWithoutLabelHasTitle.length === 0 || cases_inputWithoutLabelHasTitle[0].hasStatus(passing))) {
+              conclusion = 'passed';
+            }
+
+            // Don't bother if it isn't passed?
+            if (conclusion === 'passed') {
+              // (1) Determine if any of the following failures apply to the element
+              //   at the selector. If so, fail the Success Criteria for that selector.
+              var element = cases_labelMustBeUnique[0].attributes.element;
+
+              // F59: Using script to make div or span a user interface control in HTML without providing a role for the control (This failure may be solved in the future using DHTML roadmap techniques.)
+              if (element.nodeName === 'DIV' || element.nodeName === 'SPAN') {
+                conclusion = 'failed';
+              }
+
+              // F20: Not updating text alternatives when changes to non-text content occur
+              // N/A for labelMustBeUnique.
+
+              // F68: Association of label and user interface controls not being programmatically determined
+
+              //   F79: Focus state of a user interface component not being programmatically determinable or no notification of change of focus state available
+              //   F86: Not providing names for each part of a multi-part form field, such as a US telephone number
+              //   F89: Using null alt on an image where the image is the only content in a link ( #59 :ok: )
+              // (2) If no Failures are detected, so if the Techniques are satisfied. If
+              //   one of the techniques is satisfed for the element at the selector,
+              //   Success Criteria is satisfied.
+              //
+              //   ARIA14: Using aria-label to provide an invisible label where a visible label cannot be used
+              //   ARIA16: Using aria-labelledby to provide a name for user interface controls
+              //
+              //   G108: Using markup features to expose the name and role, allow user-settable properties to be directly set, and provide notification of changes using technology-specific techniques below:
+              //   AND
+              //
+              //   H91: Using HTML form controls and links ( #66 )
+              //   H44: Using label elements to associate text labels with form controls
+              //   H64: Using the title attribute of the frame and iframe elements ( #65 )
+              //   H65: Using the title attribute to identify form controls when the label element cannot be used ( #64 )
+              //   H88: Using HTML according to spec ( #86 )
+            }
           }
-
-          // (1) Determine if any of the following failures apply to the element
-          //   at the selector. If so, fail the Success Criteria for that selector.
-          //
-          //   F59: Using script to make div or span a user interface control in HTML without providing a role for the control (This failure may be solved in the future using DHTML roadmap techniques.)
-          //   F20: Not updating text alternatives when changes to non-text content occur
-          //   F68: Association of label and user interface controls not being programmatically determined
-          //   F79: Focus state of a user interface component not being programmatically determinable or no notification of change of focus state available
-          //   F86: Not providing names for each part of a multi-part form field, such as a US telephone number
-          //   F89: Using null alt on an image where the image is the only content in a link ( #59 :ok: )
-          // (2) If no Failures are detected, so if the Techniques are satisfied. If
-          //   one of the techniques is satisfed for the element at the selector,
-          //   Success Criteria is satisfied.
-          //
-          //   ARIA14: Using aria-label to provide an invisible label where a visible label cannot be used
-          //   ARIA16: Using aria-labelledby to provide a name for user interface controls
-          //
-          //   G108: Using markup features to expose the name and role, allow user-settable properties to be directly set, and provide notification of changes using technology-specific techniques below:
-          //   AND
-          //
-          //   H91: Using HTML form controls and links ( #66 )
-          //   H44: Using label elements to associate text labels with form controls
-          //   H64: Using the title attribute of the frame and iframe elements ( #65 )
-          //   H65: Using the title attribute to identify form controls when the label element cannot be used ( #64 )
-          //   H88: Using HTML according to spec ( #86 )
         }
         // Add the case to the Success Criteria.
         sc.addConclusion(conclusion, _case);
