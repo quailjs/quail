6b7770d41ac75b84368433b0aef52a745858f957
Théodore Biadala <theodore@biadala.net>
2014-07-12 15:03:53 +0900

    Refactor languageDirAttributeIsUsed test to limit jQuery duplication and optimize some code paths

--- src/js/custom/languageDirAttributeIsUsed.js
+++ src/js/custom/languageDirAttributeIsUsed.js
@@ -1,46 +1,43 @@
 quail.languageDirAttributeIsUsed = function(quail, test, Case) {
-  var $scope = test.get('$scope');
-  var currentDirection = ($scope.attr('dir')) ? $scope.attr('dir').toLowerCase() : 'ltr';
-  var oppositeDirection = (currentDirection === 'ltr') ? 'rtl' : 'ltr';
+
   var textDirection = quail.components.language.textDirection;
-  $scope.each(function () {
-    var $local = $(this);
-    $local.find(quail.textSelector).each(function() {
-      var $el = $(this);
-      if ($el.attr('dir')) {
-        currentDirection = $el.attr('dir').toLowerCase();
-      }
-      else {
-        currentDirection = ($el.parent('[dir]').first().attr('dir')) ? $el.parent('[dir]').first().attr('dir').toLowerCase() : currentDirection;
-      }
-      if (typeof textDirection[currentDirection] === 'undefined') {
-        currentDirection = 'ltr';
-      }
-      oppositeDirection = (currentDirection === 'ltr') ? 'rtl' : 'ltr';
-      var text = quail.getTextContents($el);
-      var matches = text.match(textDirection[oppositeDirection]);
-      if (!matches) {
-        return;
-      }
-      matches = matches.length;
-      $(this).find('[dir=' + oppositeDirection + ']').each(function() {
-        var childMatches = $el.text().match(textDirection[oppositeDirection]);
-        if (childMatches) {
-          matches = matches - childMatches.length;
-        }
-      });
-      var _case = test.add(Case({
-        element: this,
-        expected: (function (element) {
-          return quail.components.resolveExpectation(element);
-        }(this))
-      }));
-      if (matches > 0) {
-        _case.set({status: 'failed'});
-      }
-      else {
-        _case.set({status: 'passed'});
+
+  function countDirAttributes() {
+    var $el = $(this);
+    var currentDirection = $el.attr('dir');
+    if (!currentDirection) {
+      var parentDir = $el.closest('[dir]').attr('dir');
+      currentDirection = parentDir || currentDirection;
+    }
+    currentDirection = currentDirection.toLowerCase();
+    if (typeof textDirection[currentDirection] === 'undefined') {
+      currentDirection = 'ltr';
+    }
+    var oppositeDirection = (currentDirection === 'ltr') ? 'rtl' : 'ltr';
+    var text = quail.getTextContents($el);
+    var textMatches = text.match(textDirection[oppositeDirection]);
+    if (!textMatches) {
+      return;
+    }
+    var matches = textMatches.length;
+    $el.find('[dir=' + oppositeDirection + ']').each(function() {
+      var childMatches = $el[0].textContent.match(textDirection[oppositeDirection]);
+      if (childMatches) {
+        matches -= childMatches.length;
       }
     });
+
+    var _case = test.add(Case({
+      element: this,
+      expected: (function (element) {
+        return quail.components.resolveExpectation(element);
+      }(this))
+    }));
+
+    _case.set({status: (matches > 0) ? 'failed' : 'passed'});
+  }
+
+  test.get('$scope').each(function () {
+    $(this).find(quail.textSelector).each(countDirAttributes);
   });
 };
