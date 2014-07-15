e1ed60dcd4741ac2965d9a32cf442c5a6d8c1de9
Jesse Renee Beach <jesse.r.beach@gmail.com>
2014-07-15 10:26:56 -0400

    Add Success Criteria 1.4.2

---
+++ src/js/lib/wcag/SuccessCriteria-1.4.2.js
@@ -0,0 +1,44 @@
+/**
+ * Success Criterion 1.4.2: Audio control
+ *
+ * @see http://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-dis-audio.html
+ */
+quail.guidelines.wcag.successCriteria['1.4.2'] = (function (quail) {
+  var sc;
+
+  // The tests that must be run in order to evaluate this Success Criteria.
+  var requiredTests = [];
+  // The set of tests that were run that pertain to this Success Criteria. This
+  // will be the union of the tests that were run and the required tests.
+  var criteriaTests = [];
+
+  /**
+   * Determines if this Success Criteria applies to the document.
+   */
+  function preEvaluator(tests) {
+    tests = tests;
+    return true;
+  }
+
+  /**
+   * Evaluates the Success Criteria.
+   */
+  function evaluator(tests) {
+    criteriaTests = sc.filterTests(tests, requiredTests);
+    // If the length of the union equals the length of the required tests,
+    // then we have the necessary tests to evaluate this success criteria.
+    if (criteriaTests.length === requiredTests.length) {
+
+    }
+  }
+
+  // Create a new SuccessCriteria and pass it the evaluation callbacks.
+  sc = quail.lib.SuccessCriteria({
+    'name': 'wcag:1.4.2',
+    'requiredTests': requiredTests,
+    preEvaluator: preEvaluator,
+    evaluator: evaluator
+  });
+
+  return sc;
+}(quail));
