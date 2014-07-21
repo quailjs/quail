1ebea173b366e636e38388fbfbd97607d024293f
Jesse Renee Beach <jesse.r.beach@gmail.com>
2014-07-21 19:34:14 -0400

    Added SC-3.2.1

---
+++ src/js/lib/wcag/SuccessCriteria-3.2.1.js
@@ -0,0 +1,44 @@
+/**
+ * Success Criterion 3.2.1: On focus
+ *
+ * @see http://www.w3.org/WAI/WCAG20/quickref/#qr-consistent-behavior-receive-focus
+ */
+quail.guidelines.wcag.successCriteria['3.2.1'] = (function (quail) {
+  var sc;
+
+  // The tests that must be run in order to evaluate this Success Criteria.
+  var requiredTests = [];
+  // The set of tests that were run that pertain to this Success Criteria. This
+  // will be the union of the tests that were run and the required tests.
+  var criteriaTests = [];
+
+  /**
+   * Determines if this Success Criteria applies to the document.
+   */
+  function preEvaluator(tests) {
+    tests = tests;
+    return true;
+  }
+
+  /**
+   * Evaluates the Success Criteria.
+   */
+  function evaluator(tests) {
+    criteriaTests = sc.filterTests(tests, requiredTests);
+    // If the length of the union equals the length of the required tests,
+    // then we have the necessary tests to evaluate this success criteria.
+    if (criteriaTests.length === requiredTests.length) {
+
+    }
+  }
+
+  // Create a new SuccessCriteria and pass it the evaluation callbacks.
+  sc = quail.lib.SuccessCriteria({
+    'name': 'wcag:3.2.1',
+    'requiredTests': requiredTests,
+    preEvaluator: preEvaluator,
+    evaluator: evaluator
+  });
+
+  return sc;
+}(quail));
