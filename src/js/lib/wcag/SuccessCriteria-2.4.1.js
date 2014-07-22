ac53688c4e21f8e710df13d2d142ba2d6346198f
Jesse Renee Beach <jesse.r.beach@gmail.com>
2014-07-21 21:17:04 -0400

    Added sc-2.4.1

---
+++ src/js/lib/wcag/SuccessCriteria-2.4.1.js
@@ -0,0 +1,43 @@
+/**
+ * Success Criterion 2.4.1: Bypass blocks
+ *
+ * @see http://www.w3.org/TR/UNDERSTANDING-WCAG20/navigation-mechanisms-skip.html
+ */
+quail.guidelines.wcag.successCriteria['2.4.1'] = (function (quail) {
+  var sc;
+
+  // The tests that must be run in order to evaluate this Success Criteria.
+  var requiredTests = [];
+  // The set of tests that were run that pertain to this Success Criteria. This
+  // will be the union of the tests that were run and the required tests.
+  var criteriaTests = [];
+
+  /**
+   * Determines if this Success Criteria applies to the document.
+   */
+  function preEvaluator() {
+    return true;
+  }
+
+  /**
+   * Evaluates the Success Criteria.
+   */
+  function evaluator(tests) {
+    criteriaTests = sc.filterTests(tests, requiredTests);
+    // If the length of the union equals the length of the required tests,
+    // then we have the necessary tests to evaluate this success criteria.
+    if (criteriaTests.length === requiredTests.length) {
+
+    }
+  }
+
+  // Create a new SuccessCriteria and pass it the evaluation callbacks.
+  sc = quail.lib.SuccessCriteria({
+    'name': 'wcag:2.4.1',
+    'requiredTests': requiredTests,
+    preEvaluator: preEvaluator,
+    evaluator: evaluator
+  });
+
+  return sc;
+}(quail));
