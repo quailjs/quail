54fb2edb9f0d386633d746d0e796950e5fa35c9d
Jesse Renee Beach <jesse.r.beach@gmail.com>
2014-07-21 21:01:38 -0400

    Added sc-3.2.4

---
+++ src/js/lib/wcag/SuccessCriteria-3.2.4.js
@@ -0,0 +1,43 @@
+/**
+ * Success Criterion 3.2.4: Consistent identification
+ *
+ * @see http://www.w3.org/TR/UNDERSTANDING-WCAG20/consistent-behavior-consistent-functionality.html
+ */
+quail.guidelines.wcag.successCriteria['3.2.4'] = (function (quail) {
+  var sc;
+
+  // The tests that must be run in order to evaluate this Success Criteria.
+  var requiredTests = [];
+  // The set of tests that were run that pertain to this Success Criteria. This
+  // will be the union of the tests that were run and the required tests.
+  var criteriaTests = [];
+
+  /**
+   * Determines if this Success Criteria applies to the document.
+   */
+  function preEvaluator() {
+    return true;
+  }
+
+  /**
+   * Evaluates the Success Criteria.
+   */
+  function evaluator(tests) {
+    criteriaTests = sc.filterTests(tests, requiredTests);
+    // If the length of the union equals the length of the required tests,
+    // then we have the necessary tests to evaluate this success criteria.
+    if (criteriaTests.length === requiredTests.length) {
+
+    }
+  }
+
+  // Create a new SuccessCriteria and pass it the evaluation callbacks.
+  sc = quail.lib.SuccessCriteria({
+    'name': 'wcag:3.2.4',
+    'requiredTests': requiredTests,
+    preEvaluator: preEvaluator,
+    evaluator: evaluator
+  });
+
+  return sc;
+}(quail));
