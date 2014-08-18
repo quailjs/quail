57052c52161fdde9dd2d3b244bfdcabc45a7edc9
Jesse Renee Beach <jesse.r.beach@gmail.com>
2014-08-18 18:30:25 -0400

    PreEvaluation test for SC 3.3.3

--- src/js/lib/wcag/SuccessCriteria-3.3.3.js
+++ src/js/lib/wcag/SuccessCriteria-3.3.3.js
@@ -16,7 +16,43 @@ quail.guidelines.wcag.successCriteria['3.3.3'] = (function (quail) {
    * Determines if this Success Criteria applies to the document.
    */
   function preEvaluator() {
-    return true;
+    var complexInputTypes = [
+      //'button', //  Defines a clickable button (mostly used with a JavaScript to activate a script)
+      'checkbox', //  Defines a checkbox
+      'color', // Defines a color picker
+      'date', //  Defines a date control (year, month and day (no time))
+      'datetime', //  Defines a date and time control (year, month, day, hour, minute, second, and fraction of a second, based on UTC time zone)
+      'datetime-local', //  Defines a date and time control (year, month, day, hour, minute, second, and fraction of a second (no time zone)
+      'email', // Defines a field for an e-mail address
+      'file', //  Defines a file-select field and a "Browse..." button (for file uploads)
+      'hidden', //  Defines a hidden input field
+      //'image', // Defines an image as the submit button
+      'month', // Defines a month and year control (no time zone)
+      'number', //  Defines a field for entering a number
+      'password', //  Defines a password field (characters are masked)
+      'radio', // Defines a radio button
+      'range', // Defines a control for entering a number whose exact value is not important (like a slider control)
+      //'reset', // Defines a reset button (resets all form values to default values)
+      'search', //  Defines a text field for entering a search string
+      //'submit', //  Defines a submit button
+      'tel', // Defines a field for entering a telephone number
+      //'text', //  Default. Defines a single-line text field (default width is 20 characters)
+      'time', //  Defines a control for entering a time (no time zone)
+      'url', // Defines a field for entering a URL
+      'week' //  Defines a week and year control (no time zone)
+    ];
+    // Testing forms.
+    //
+    // If any of the complex form types are present in the document, this
+    // success criteria applies.
+    if (document.querySelectorAll('form').length) {
+      return complexInputTypes.some(function (type) {
+        return !!document.querySelectorAll('[type="' + type + '"]').length;
+      });
+    }
+    else {
+      return false;
+    }
   }
 
   /**
