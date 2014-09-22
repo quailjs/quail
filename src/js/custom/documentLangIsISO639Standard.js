93be30fe119620e2ce51619982922a3e29b4b6b1
Wilco Fiers <wilco@pangeadc.nl>
2014-09-22 10:48:49 +0200

    documentLangIsISO improved

--- src/js/custom/documentLangIsISO639Standard.js
+++ src/js/custom/documentLangIsISO639Standard.js
@@ -10,16 +10,32 @@ quail.documentLangIsISO639Standard = function(quail, test, Case) {
       $element.data('expected')
   });
 
+  var langAttr = $element.attr('lang');
+  var matchedLang = false; // Check to see if a languagecode was matched
+
   test.add(_case);
-  if (!$element.is('html') || !$element.attr('lang')) {
+  if (!$element.is('html') || typeof langAttr === 'undefined') {
     _case.set({
       'status' : 'notApplicable'
     });
   } else {
-    _case.set({
-      'status': (quail.strings.languageCodes.indexOf($element.attr('lang')) !== 0) ?
-        'failed' :
-        'passed'
+    // Loop over all language codes, checking if the current lang attribute starts
+    // with a value that's in the languageCodes array
+    $.each(quail.strings.languageCodes, function (i, currentLangCode) {
+      if (!matchedLang && langAttr.indexOf(currentLangCode) === 0) {
+        matchedLang = true;
+      }
     });
+    
+    if (!matchedLang) {
+      _case.set({'status': 'failed'});
+      
+    } else if (langAttr.match(/^[a-z]{2}(-[A-Z]{2})?$/) === null) {
+      _case.set({'status': 'failed'});
+      
+    } else {
+      _case.set({'status': 'passed'});
+    }
   }
+
 };
