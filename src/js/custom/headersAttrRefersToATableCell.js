beff3afab2fa07419f3ff1c400badbc7747dac62
Marco Alting <marco@alting-multimedia.nl>
2014-10-01 13:06:31 +0200

    - Fixed test

--- src/js/custom/headersAttrRefersToATableCell.js
+++ src/js/custom/headersAttrRefersToATableCell.js
@@ -1,37 +1,38 @@
 quail.headersAttrRefersToATableCell = function( quail, test, Case ) {
 
   // Table cell headers without referred ids
-  test.get( '$scope' ).find( 'td, th' ).each( function() {
+  test.get( '$scope' ).find( 'table' ).each( function() {
 
     var element = this,
-      _case = Case({
-      element: element,
-      expected: $(this).closest('.quail-test').data('expected')
-    });
-    test.add(_case);
+      _case = Case( {
+        element: element,
+        expected: $( this ).closest( '.quail-test' ).data( 'expected' )
+      } );
+    test.add( _case );
 
-    if ( !$( element ).attr( 'headers' )) {
+
+    if ( $( element ).find( '[headers]' ).length === 0 ) {
       _case.set( {
         'status': 'inapplicable'
       } );
       return;
+    } else {
+      $( element ).find( 'th[headers], td[headers]' ).each( function() {
+        var headers = $( this ).attr( 'headers' ).split( /\s+/ );
+        $.each( headers, function( index, item ) {
+          if (item === "" || $( element ).find( 'th#' + item + ',td#' + item ).length > 0 ) {
+            _case.set( {
+              'status': 'passed'
+            } );
+            return;
+          } else {
+            _case.set( {
+              'status': 'failed'
+            } );
+            return;
+          }
+        } );
+      } );
     }
-
-    var headers = $(element).attr('headers').split(/\s+/);
-
-    $.each( headers, function( index, item ) {
-      if ( $( element ).closest( 'table' ).find( 'th#' + item + ',td#' + item ).length > 0  ) {
-        _case.set( {
-          'status': 'passed'
-        } );
-      } else {
-        _case.set( {
-          'status': 'failed'
-        } );
-      }
-      window.console.log($( element ).closest( 'div' ).attr('data-expected'),_case.attributes.expected,_case.attributes.expected);
-    });
-
-
   } );
 };
