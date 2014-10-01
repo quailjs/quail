655c26d0b81280f034e4e30b1c3b7f27e04a9963
Marco Alting <marco@alting-multimedia.nl>
2014-10-01 13:37:29 +0200

    - WIP

--- src/js/custom/aInPhasADistinctStyle.js
+++ src/js/custom/aInPhasADistinctStyle.js
@@ -1,8 +1,14 @@
 quail.aInPhasADistinctStyle = function( quail, test, Case ) {
   test.get( '$scope' ).find( 'p a' ).each( function() {
-    var _case = test.add( Case( {
-      element: this
-    } ) );
+    var _case = Case( {
+      element: this,
+      expected: $(this).closest('.quail-test').data('expected')
+    } );
+
+    window.console.log("TEST");
+    window.console.log(_case);
+
+    test.add( _case );
     var expected = $( this ).closest( '.quail-test' ).data( 'expected' );
 
 //    if (!$(this ).attr('href') || quail.cleanString($(this).attr('href')) === "" ) {
