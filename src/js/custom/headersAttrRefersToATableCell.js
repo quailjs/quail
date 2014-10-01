6ce2cb1c8f10b4c5afba95c695a29da94018498b
Marco Alting <marco@alting-multimedia.nl>
2014-10-01 10:41:57 +0200

    - Added tests

--- src/js/custom/headersAttrRefersToATableCell.js
+++ src/js/custom/headersAttrRefersToATableCell.js
@@ -3,28 +3,34 @@ quail.headersAttrRefersToATableCell = function( quail, test, Case ) {
   // Table cell headers without referred ids
   test.get( '$scope' ).find( 'td, th' ).each( function() {
 
-    var _case = Case({
-      element: this,
+    var element = this,
+      _case = Case({
+      element: element,
       expected: $(this).closest('.quail-test').data('expected')
     });
     test.add(_case);
 
-    if ( !$( this ).attr( 'headers' )) {
+    if ( !$( element ).attr( 'headers' )) {
       _case.set( {
         'status': 'inapplicable'
       } );
-      return; // Stop each loop
+      return;
     }
 
-    if ( $( this ).attr( 'headers' )) {
-      _case.set( {
-        'status': 'passed'
-      } );
-    }  else{
-      _case.set( {
-        'status': 'failed'
-      } );
-    }
+    var headers = $(element).attr('headers').split(/\s+/);
+
+    $.each( headers, function( index, item ) {
+      if ( $( element ).closest( 'table' ).find( 'th#' + item + ',td#' + item ).length > 0  ) {
+        _case.set( {
+          'status': 'passed'
+        } );
+      } else {
+        _case.set( {
+          'status': 'failed'
+        } );
+      }
+      window.console.log($( element ).closest( 'div' ).attr('data-expected'),_case.attributes.expected,_case.attributes.expected);
+    });
 
 
   } );
