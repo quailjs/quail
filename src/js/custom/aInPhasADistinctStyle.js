a56d37b4c1018bb665b1174990be48a997a434d5
Marco Alting <marco@alting-multimedia.nl>
2014-10-01 17:01:49 +0200

    - Added test criteria to Javascript test

--- src/js/custom/aInPhasADistinctStyle.js
+++ src/js/custom/aInPhasADistinctStyle.js
@@ -1,26 +1,52 @@
 quail.aInPHasADistinctStyle = function( quail, test, Case ) {
 
-  window.console.log(quail);
+  function hasBorder( element ) {
+    if ( (element.outerWidth() - element.innerWidth() > 0) || (element.outerHeight() - element.innerHeight() > 0) ) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
 
   test.get( '$scope' ).find( 'p a' ).each( function() {
-    window.console.log(test.get("$scope"));
     var _case = Case( {
       element: this,
-      expected: $(this).closest('.quail-test').data('expected')
+      expected: $( this ).closest( '.quail-test' ).data( 'expected' )
     } );
-
-    window.console.log("TEST");
-    window.console.log(_case);
 
     test.add( _case );
     var expected = $( this ).closest( '.quail-test' ).data( 'expected' );
 
-//    if (!$(this ).attr('href') || quail.cleanString($(this).attr('href')) === "" ) {
-    _case.set( {
-      'expected': expected,
-      'status': 'inapplicable'
-    } );
-//    }
+
+    if ( !$( this ).attr( 'href' ) || quail.cleanString( $( this ).attr( 'href' ) ) === "" ) {
+      _case.set( {
+        'expected': expected,
+        'status': 'inapplicable'
+      } );
+      return;
+    }
+
+    if (
+      $( this ).css( 'text-decoration' ) === 'underline' ||
+      $( this ).css( 'display' ) === 'block' ||
+      ($( this ).css( 'background-color' ) !== 'rgba(0, 0, 0, 0)' && $( this ).css( 'background-color' ) !== $( this ).closest( 'p' ).css( 'background-color' )) ||
+      hasBorder( $( this ) ) ||
+      $( this ).children( 'img' ).length > 0
+      ) {
+      _case.set( {
+        'expected': expected,
+        'status': 'passed'
+      } );
+      return;
+    } else {
+      _case.set( {
+        'expected': expected,
+        'status': 'failed'
+      } );
+      return;
+    }
 
   } );
-};
+}
+;
