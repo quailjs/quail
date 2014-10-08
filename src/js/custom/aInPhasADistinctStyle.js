4ea1710e089c7cb0cb46ccb0090b552a666e079a
Marco Alting <marco@alting-multimedia.nl>
2014-10-08 14:22:37 +0200

    - Fixed jslint error

--- src/js/custom/aInPhasADistinctStyle.js
+++ src/js/custom/aInPhasADistinctStyle.js
@@ -1,52 +1,48 @@
-quail.aInPHasADistinctStyle = function( quail, test, Case ) {
+quail.aInPHasADistinctStyle=function(quail, test, Case){
 
-  function hasBorder( element ) {
-    if ( (element.outerWidth() - element.innerWidth() > 0) || (element.outerHeight() - element.innerHeight() > 0) ) {
+  function hasBorder(element){
+    if ((element.outerWidth() - element.innerWidth() > 0) || (element.outerHeight() - element.innerHeight() > 0)) {
       return true;
     }
     else {
       return false;
     }
-  };
+  }
 
-  test.get( '$scope' ).find( 'p a' ).each( function() {
-    var _case = Case( {
+  test.get('$scope').find('p a').each(function(){
+    var _case=Case({
       element: this,
-      expected: $( this ).closest( '.quail-test' ).data( 'expected' )
-    } );
+      expected: $(this).closest('.quail-test').data('expected')
+    });
 
-    test.add( _case );
-    var expected = $( this ).closest( '.quail-test' ).data( 'expected' );
+    test.add(_case);
+    var expected=$(this).closest('.quail-test').data('expected');
 
-
-    if ( !$( this ).attr( 'href' ) || quail.cleanString( $( this ).attr( 'href' ) ) === "" ) {
-      _case.set( {
+    if (!$(this).attr('href') || quail.cleanString($(this).attr('href')) === "") {
+      _case.set({
         'expected': expected,
         'status': 'inapplicable'
-      } );
+      });
       return;
     }
-
     if (
-      $( this ).css( 'text-decoration' ) === 'underline' ||
-      $( this ).css( 'display' ) === 'block' ||
-      ($( this ).css( 'background-color' ) !== 'rgba(0, 0, 0, 0)' && $( this ).css( 'background-color' ) !== $( this ).closest( 'p' ).css( 'background-color' )) ||
-      hasBorder( $( this ) ) ||
-      $( this ).children( 'img' ).length > 0
+      $(this).css('text-decoration') === 'underline' ||
+      $(this).css('display') === 'block' ||
+      ($(this).css('background-color') !== 'rgba(0, 0, 0, 0)' && $(this).css('background-color') !== $(this).closest('p').css('background-color')) ||
+      hasBorder($(this)) ||
+      $(this).children('img').length > 0
       ) {
-      _case.set( {
+      _case.set({
         'expected': expected,
         'status': 'passed'
-      } );
+      });
       return;
     } else {
-      _case.set( {
+      _case.set({
         'expected': expected,
         'status': 'failed'
-      } );
+      });
       return;
     }
-
-  } );
-}
-;
+  });
+};
