548172deb2ef53d1ca2cc85c07bfc19de9675bae
Marco Alting <marco@alting-multimedia.nl>
2014-10-09 09:52:38 +0200

    - Added inapplicable test if only 1 element is in the test with an id attribute.

--- src/js/custom/documentIDsMustBeUnique.js
+++ src/js/custom/documentIDsMustBeUnique.js
@@ -1,5 +1,5 @@
 quail.documentIDsMustBeUnique = function(quail, test, Case) {
-  var ids = {};
+
 
   test.get('$scope').each(function(){
     if($(this).children().length === 0) {
@@ -17,24 +17,41 @@ quail.documentIDsMustBeUnique = function(quail, test, Case) {
       expected: $(this).closest('.quail-test').data('expected')
     }));
   });
-  test.get('$scope').find('[id]').each(function() {
-    var _case = Case({
-      element: this,
-      expected: (function (element) {
-        return quail.components.resolveExpectation(element);
-      }(this))
+
+  test.get('$scope').each(function(){
+    var ids = {};
+
+
+    $(this).find('[id]').each(function() {
+
+      var _case = Case({
+        element: this,
+        expected: (function (element) {
+          return quail.components.resolveExpectation(element);
+        }(this))
+      });
+      test.add(_case);
+
+      window.console.log(ids, typeof ids[$(this).attr('id')], Object.keys(ids).length);
+
+      if(typeof ids[$(this).attr('id')] === 'undefined' && Object.keys(ids).length === 0){
+        _case.set({
+          'status': 'inapplicable'
+        });
+        ids[$(this).attr('id')] = $(this).attr('id');
+      }else if (typeof ids[$(this).attr('id')] === 'undefined') {
+        _case.set({
+          'status': 'passed'
+        });
+        ids[$(this).attr('id')] = $(this).attr('id');
+      }
+      else {
+        _case.set({
+          'status': 'failed'
+        });
+      }
     });
-    test.add(_case);
-    if (typeof ids[$(this).attr('id')] === 'undefined') {
-      _case.set({
-        'status': 'passed'
-      });
-      ids[$(this).attr('id')] = $(this).attr('id');
-    }
-    else {
-      _case.set({
-        'status': 'failed'
-      });
-    }
+
   });
+
 };
