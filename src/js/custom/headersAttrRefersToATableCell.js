2031cd6e4fab31fa640e762cfa10ac8b511c70c3
Wilco Fiers <wilco@pangeadc.nl>
2014-10-13 11:24:21 +0200

    headersAttrRef performance improvement

--- src/js/custom/headersAttrRefersToATableCell.js
+++ src/js/custom/headersAttrRefersToATableCell.js
@@ -3,35 +3,32 @@ quail.headersAttrRefersToATableCell = function( quail, test, Case ) {
   // Table cell headers without referred ids
   test.get( '$scope' ).find( 'td, th' ).each( function() {
 
-    var element = this,
+    var $this = $(this),
       _case = Case({
-      element: element,
-      expected: $(this).closest('.quail-test').data('expected')
+      element: this,
+      expected: $this.closest('.quail-test').data('expected')
     });
     test.add(_case);
 
-    if ( !$( element ).attr( 'headers' )) {
+    if ( !$this.attr( 'headers' )) {
       _case.set( {
         'status': 'inapplicable'
       } );
       return;
     }
 
-    var headers = $(element).attr('headers').split(/\s+/);
-
-    $.each( headers, function( index, item ) {
-      if ( $( element ).closest( 'table' ).find( 'th#' + item + ',td#' + item ).length > 0  ) {
-        _case.set( {
+    var headers = $this.attr('headers').split(/\s+/);
+    var table = $this.closest('table');
+    $.each(headers, function(index, item) {
+      if (table.find('th#' + item + ',td#' + item).length > 0) {
+        _case.set({
           'status': 'passed'
-        } );
+        });
       } else {
-        _case.set( {
+        _case.set({
           'status': 'failed'
-        } );
+        });
       }
-      window.console.log($( element ).closest( 'div' ).attr('data-expected'),_case.attributes.expected,_case.attributes.expected);
     });
-
-
-  } );
+  });
 };
