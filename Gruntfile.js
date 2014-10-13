4a0cc2198c8bce3dfa1356c3785b1fc365579428
Jesse Renee Beach <jesse.r.beach@gmail.com>
2014-10-13 16:01:03 -0400

    Just run phantomjs as the default karma browser

--- Gruntfile.js
+++ Gruntfile.js
@@ -111,7 +111,8 @@ module.exports = function(grunt) {
     karma: {
       unit: {
         configFile: 'karma.conf.js',
-        singleRun: true
+        singleRun: true,
+        browsers: ['PhantomJS']
       }
     },
     jshint: {
