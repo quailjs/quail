1e89fe1b4aedb54bc127bdf0c8a3cfec85f07f7c
Marco Alting <marco@alting-multimedia.nl>
2014-10-14 11:33:52 +0200

    - Added extra tests

--- src/js/custom/aInPhasADistinctStyle.js
+++ src/js/custom/aInPhasADistinctStyle.js
@@ -1,5 +1,22 @@
 quail.aInPHasADistinctStyle=function(quail, test, Case){
 
+  /**
+   * Function for just testing the elements text, without checking text of children
+   * @returns {*|jQuery}
+   */
+  jQuery.fn.getElementText=function(){
+    return $(this).clone()
+      .children()
+      .remove()
+      .end()
+      .text();
+  };
+
+  /**
+   * Checks if an element has a border set
+   * @param element
+   * @returns {boolean}
+   */
   function hasBorder(element){
     if ((element.outerWidth() - element.innerWidth() > 0) || (element.outerHeight() - element.innerHeight() > 0)) {
       return true;
@@ -7,6 +24,23 @@ quail.aInPHasADistinctStyle=function(quail, test, Case){
     else {
       return false;
     }
+  }
+
+  /**
+   * Checks if the parent element is a native style element
+   * @param parentElement
+   * @returns {boolean}
+   */
+  function hasNativeStyle(parentElement){
+    var nativeStyles=['b', 'strong', 'em', 'u'];
+    var result=false;
+
+    $.each(nativeStyles, function(index, item){
+      if (parentElement.is(item) && parentElement.getElementText() === "") {
+        result=true;
+      }
+    });
+    return result;
   }
 
   test.get('$scope').find('p a').each(function(){
@@ -17,7 +51,6 @@ quail.aInPHasADistinctStyle=function(quail, test, Case){
 
     test.add(_case);
     var expected=$(this).closest('.quail-test').data('expected');
-
     if (!$(this).attr('href') || quail.cleanString($(this).attr('href')) === "") {
       _case.set({
         'expected': expected,
@@ -26,6 +59,7 @@ quail.aInPHasADistinctStyle=function(quail, test, Case){
       return;
     }
     if (
+      hasNativeStyle($(this).parent()) ||
       $(this).css('text-decoration') === 'underline' ||
       $(this).css('display') === 'block' ||
       ($(this).css('background-color') !== 'rgba(0, 0, 0, 0)' && $(this).css('background-color') !== $(this).closest('p').css('background-color')) ||
