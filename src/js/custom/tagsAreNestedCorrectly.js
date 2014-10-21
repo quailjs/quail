e17c3ae5aa6d36bf103f84c3d1c68397d2a8b003
Marco Alting <marco@alting-multimedia.nl>
2014-10-21 17:35:16 +0200

    - Added htmlTagValidator - Created tests for tag validation WIP

--- src/js/custom/tagsAreNestedCorrectly.js
+++ src/js/custom/tagsAreNestedCorrectly.js
@@ -1,51 +1,38 @@
-quail.tagsAreNestedCorrectly = function(quail, test, Case) {
-  quail.components.htmlSource.getHtml(function(html, parsed) {
-    quail.components.htmlSource.traverse(parsed, function(element) {
+quail.tagsAreNestedCorrectly=function(quail, test, Case){
 
-      if (element.type !== 'tag' || !$.isArray(element.selector)) {
-        return;
-      }
-      var selector;
-      // Use the element's ID if it has one.
-      if (/#/.test(element.selector.slice(-1)[0])) {
-        selector = element.selector.slice(-1)[0];
-      }
-      // Otherwise construct the path from the selector pieces.
-      else {
-        selector = element.selector.join(' > ');
-      }
+  quail.components.htmlSource.getHtml(function(html) {
+    var regex = /<div class="quail-test"[\S\s]*?>([\S\s]*?)<\/div>/gmi;
+    var tests = [];
+    var testBlocks = test.get('scope');
 
-      // If selector matches a DOM node in the scope, get a reference to the
-      // node, otherwise we'll have to back off to just giving details about
-      // the node. This might happen if the DOM in the real page is
-      // transformed too drastically from the parsed DOM.
-      var node = $(selector, test.get('$scope')).get(0);
-      if (!node) {
-        node = element.raw || selector;
-      }
+    var match;
+    while((match = regex.exec(html)) !== null){
+      window.console.log(match[1]);
+      tests.push(match[1]);
+    }
 
-      if ((!element.closingTag &&
-        quail.selfClosingTags.indexOf(element.name.toLowerCase()) === -1 &&
-        quail.optionalClosingTags.indexOf(element.name.toLowerCase()) === -1)) {
+    window.console.log(tests);
 
-        test.add(Case({
-          element: node,
-          // Only attempt to get an expectation for the testrunner if the node
-          // is a DOM node.
-          expected: (typeof node === 'object') && (node.nodeType === 1) && $(node).closest('.quail-test').data('expected') || null,
-          status: 'failed'
-        }));
-      }
-      else {
+    $.each(tests, function(index, testHTML){
 
-        test.add(Case({
-          element: node,
-          // Only attempt to get an expectation for the testrunner if the node
-          // is a DOM node.
-          expected:(typeof node === 'object') && (node.nodeType === 1) && $(node).closest('.quail-test').data('expected') || null,
-          status: 'passed'
-        }));
+
+      var testBlock = testBlocks[index];
+      var _case=Case({
+        element: this,
+        expected: $(testBlock).data('expected')
+      });
+      test.add(_case);
+
+      if(quail.components.htmlTagValidator(testHTML)){
+        _case.set({
+          'status': 'passed'
+        });
+      }else{
+        _case.set({
+          'status': 'failed'
+        });
       }
+      return;
     });
   });
 };
