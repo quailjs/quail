17c83678148cecc931d5e9cb71e428e9b4ffcbaf
Jesse Renee Beach <jesse.r.beach@gmail.com>
2014-10-26 23:17:07 -0400

    Moving selenium setup files into their own module

--- test/selenium-test/Spec.js
+++ test/selenium-test/Spec.js
@@ -1,46 +1,13 @@
 var chai = require('chai');
-var webdriver = require('selenium-webdriver' );
+var driverFactory = require('./node_modules/driverFactory/driverFactory');
 var FIXTURE = 'http://localhost:9000/index.html';
 var expect = chai.expect;
 var should = chai.should();
 
-/**
- * creates a webdriver client
- * @param callBack or promise
- */
-function createClient (callBack) {
-  var serverConfig = 'http://127.0.0.1:4444/wd/hub';
-  var capabilities = {
-    silent: true, // maybe output more for tests?
-    browserName: 'phantomjs',
-    javascriptEnabled: true,
-    takesScreenshot: true,
-    databaseEnabled: false,
-    cssSelectorsEnabled:true,
-    webStorageEnabled: true
-  };
-
-  var driver = new webdriver
-    .Builder()
-    .usingServer(serverConfig)
-    .withCapabilities(capabilities)
-    .build();
-
-  if (typeof callBack === 'function') {
-    return callBack (driver);
-  }
-  else if ( typeof callBack === 'object' && typeof callBack.resolve === 'function' ) {
-    return callBack.resolve(driver);
-  }
-  else {
-    return driver;
-  }
-}
-
 describe('The driver', function () {
   var _driver;
   beforeEach(function () {
-    createClient(function (driver) {
+    driverFactory.createClient(function (driver) {
       _driver = driver;
     });
   });
@@ -49,15 +16,7 @@ describe('The driver', function () {
   });
 
   it('exists', function (done) {
-    var s = '';
-    s += 'var s = document.createElement("script");';
-    s += 's.type = "text/javascript";'
-    s += 's.innerText = "var a = 5;";';
-    s += 'var domel = document.getElementsByTagName("script")[0];';
-    s += 'domel.parentNode.insertBefore(s, domel);';
     expect(_driver).to.exist;
-    _driver.get('http://quailpages/forms/simple-form.html');
-    _driver.executeScript(s);
     _driver.getPageSource().then(function (source) {
       console.log(source);
       done();
