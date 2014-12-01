d9f197dd79347badd307e4842f9aaec4d03123c5
Jesse Renee Beach <jesse.r.beach@gmail.com>
2014-11-30 21:30:33 -0500

    Adding a test for aMustHaveTitleSpec

---
+++ test/selenium-test/specs/aMustHaveTitle/aMustHaveTitleSpec.js
@@ -0,0 +1,19 @@
+describe('The form test', function () {
+  before(h.setup('http://quailpages/forms/simple-form.html'));
+
+  it('should have a client', function () {
+    expect(this.client).to.exist;
+  });
+
+  it('should have results', function () {
+    expect(this.results).to.exist;
+  });
+
+  it('should have tests', function () {
+    expect(this.results.stats.tests).to.equal(250);
+  });
+
+  it('should have cases', function () {
+    expect(this.results.stats.cases).to.equal(280);
+  });
+});
