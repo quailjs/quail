3c056cff3eb8298dc5ea681ff1dbb1c528363b24
Wilco Fiers <wilco@pangeadc.nl>
2014-12-02 14:31:29 +0100

    Redesigned video precondition

--- src/js/custom/videoMayBePresent.js
+++ src/js/custom/videoMayBePresent.js
@@ -1,65 +1,59 @@
 quail.videoMayBePresent=function(quail, test, Case){
 
-  function getSource(element){
-    if (element.is('a')) {
-      return element.attr('href');
-    }
-
-    if (element.find('source').attr('src') !== undefined) {
-      return element.find('source').attr('src');
-    }
-
-    if (element.attr('src') !== undefined) {
-      return element.attr('src');
-    }
-  }
+  var videoExtensions = ['webm', 'flv', 'ogv', 'ogg', 'avi', 'mov', 'qt', 'wmv', 'asf',
+  'mp4', 'm4p', 'm4v', 'mpg', 'mp2', 'mpeg', 'mpg', 'mpe', 'mpv', 'm2v', '3gp', '3g2'];
+  var videoHosts = ['//www.youtube.com/embed/', '//player.vimeo.com/video/'];
 
   test.get('$scope').each(function(){
-    var testableElements=$(this).find('video, object, iframe, a');
+    var $this = $(this);
+    var hasCase = false; // Test if a case has been created
 
-    if (testableElements.length === 0) {
-      var _case=Case({
+    // video elm is definately a video, and objects could be too.
+    $this.find('object, video').each(function () {
+      hasCase = true;
+      test.add(Case({
+        element: this,
+        expected: $(this).closest('.quail-test').data('expected'),
+        status: 'cantTell'
+      }));
+    });
+
+    // Links refering to files with an video extensions are probably video
+    // though the file may not exist. 
+    $this.find('a[href]').each(function () {
+      var $this = $(this);
+      var extension = $this.attr('href').split('.').pop();
+      if ($.inArray(extension, videoExtensions) !== -1) {
+        hasCase = true;
+        test.add(Case({
+          element: this,
+          expected: $this.closest('.quail-test').data('expected'),
+          status: 'cantTell'
+        }));
+      }
+    });
+
+    // some iframes with URL's of known video providers are also probably videos
+    $this.find('iframe').each(function () {
+      if (this.src.indexOf(videoHosts[0]) !== -1 ||
+      this.src.indexOf(videoHosts[1]) !== -1) {
+        hasCase = true;
+        test.add(Case({
+          element: this,
+          expected: $this.closest('.quail-test').data('expected'),
+          status: 'cantTell'
+        }));
+      }
+    });
+
+    // if no case was added, return inapplicable
+    if (!hasCase) {
+      test.add(Case({
         element: this,
         status: 'inapplicable',
         expected: $(this).closest('.quail-test').data('expected')
-      });
-      test.add(_case);
+      }));
     }
 
-    testableElements.each(function(){
-      var fileTypes=['webm', 'flv', 'ogv', 'ogg', 'avi', 'mov', 'qt', 'wmv', 'asf', 'mp4', 'm4p', 'm4v', 'mpg', 'mp2', 'mpeg', 'mpg', 'mpe', 'mpv', 'm2v', '3gp', '3g2'];
-      var _case=Case({
-        element: this,
-        expected: $(this).closest('.quail-test').data('expected')
-      });
-      test.add(_case);
-
-      var source = getSource($(this));
-      if (source && $.inArray(source.split('.').pop(), fileTypes) > -1) {
-        _case.set({
-          'status': 'cantTell'
-        });
-        return;
-      }
-      if($(this).find('param[name="flashvars"]').attr('value') !== undefined){
-
-        source = $(this).find('param[name="flashvars"]').attr('value');
-        $.each(fileTypes, function(index, item){
-          if(source.indexOf("." + item) > -1) {
-            _case.set({
-              'status': 'cantTell'
-            });
-            return false;
-          }
-        });
-        return;
-      }
-      if(source.indexOf('//www.youtube.com/embed/') > -1 || source.indexOf('//player.vimeo.com/video/') > -1){
-        _case.set({
-          'status': 'cantTell'
-        });
-        return;
-      }
-    });
   });
 };
