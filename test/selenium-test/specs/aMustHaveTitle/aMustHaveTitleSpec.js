c4074c2581cf0d08308826f474b22a97a74cee7d
Jesse Renee Beach <jesse.r.beach@gmail.com>
2014-12-07 16:44:48 -0500

    The first steps to setting up the assessments and evaluation using Promises.

--- test/selenium-test/specs/aMustHaveTitle/aMustHaveTitleSpec.js
+++ test/selenium-test/specs/aMustHaveTitle/aMustHaveTitleSpec.js
@@ -1,33 +1,40 @@
 describe('assessment: aMustHaveTitle', function () {
   var quailResults;
 
-  before(quailTestRunner.setup('http://localhost:9999/aMustHaveTitle/aMustHaveTitle.html', [
-    'aMustHaveTitle'
-  ]));
+  // Evaluate the test page with Quail.
+  before('load webdrivers and run evaluations with Quail', function () {
+    return quailTestRunner.setup('http://localhost:9999/aMustHaveTitle/aMustHaveTitle.html', [
+        'aMustHaveTitle'
+      ])
+      .spread(function (client, assessments, _quailResults_) {
+        debugger;
+        quailResults = _quailResults_;
+      });
+  });
 
   it('should have a client', function () {
     expect(this.client).to.exist;
   });
 
-  it('should have results', function () {
-    expect(this.results).to.exist;
-  });
-
-  it('should have tests', function () {
-    expect(this.results.stats.tests).to.equal(1);
-  });
-
-  it('should have cases', function () {
-    expect(this.results.stats.cases).to.equal(1);
-    expect(this.results.tests).to.include.keys('aMustHaveTitle');
-    expect(this.results.tests);
-  });
-
-  it('should return the proper assessment for the tests', function () {
-    expect(this.results.tests['aMustHaveTitle'].cases).to.deep.equal([{
-      html: "<a href=\"dogs.html\" class=\"quail-failed-element\">information about dogs</a>",
-      selector: "a[href=\"dogs.html\"].quail-failed-element",
-      status: "failed"
-    }]);
-  });
+//  it('should have results', function () {
+//    expect(quailResults).to.exist;
+//  });
+//
+//  it('should have tests', function () {
+//    expect(quailResults.stats.tests).to.equal(1);
+//  });
+//
+//  it('should have cases', function () {
+//    expect(quailResults.stats.cases).to.equal(1);
+//    expect(quailResults.tests).to.include.keys('aMustHaveTitle');
+//    expect(quailResults.tests);
+//  });
+//
+//  it('should return the proper assessment for the tests', function () {
+//    expect(quailResults.tests['aMustHaveTitle'].cases).to.deep.equal([{
+//      html: "<a href=\"dogs.html\" class=\"quail-failed-element\">information about dogs</a>",
+//      selector: "a[href=\"dogs.html\"].quail-failed-element",
+//      status: "failed"
+//    }]);
+//  });
 });
--- test/selenium-test/testRunner.js
+++ test/selenium-test/testRunner.js
@@ -9,9 +9,12 @@ var selenium = require('selenium-standalone');
 var Mocha = require('mocha');
 var should = require('should');
 var chai = require('chai');
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
 global.expect = chai.expect;
 global.assert = chai.assert;
 var glob = require('glob');
+var Q = require('q'); // https://github.com/kriskowal/q
 var mochaRunner, seleniumServer, webdriver, client, specFiles;
 
 // The root path of the HTTP server at port 8888.
@@ -93,7 +96,7 @@ function runSpecs (err, assessmentsJSON) {
   }
 
   // Set up the Mocha test runs.
-  specFiles = __dirname + '/specs/**/*Spec.js';
+  specFiles = __dirname + '/specs/aMustHaveTitle/*Spec.js';
 
   // Gather the spec files and add them to the Mocha run.
   glob(specFiles, function (error, files) {
@@ -106,9 +109,9 @@ function runSpecs (err, assessmentsJSON) {
         return shutdownTestRunner(failures);
       }
 
-      debugger;
-
-      return client.end(shutdownTestRunner(failures));
+      return client.end(function (err, ret) {
+        shutdownTestRunner(failures);
+      });
     }
 
     mochaRunner = new Mocha({
@@ -141,272 +144,65 @@ function runSpecs (err, assessmentsJSON) {
         }
       };
     },
+    // The setup process involves:
+    // 1. Preparing the list of assessments
+    // 2. Initiating a webdriver client for the test suite
+    // 3. Loading loading the indicated URL in a Selenium browser instance.
+    //   a. jQuery
+    //   b. Quail
+    // 4. Evaluating the page with Quail and returning the results object.
+    //   a. The evaluation of the page either resolves or rejects the promise
+    //      object returned to the testing suite by the setup method.
     setup: function(url, indicatedAssessments, newSession) {
-
-      // Filter the list of assessments if the Spec indicates a subset.
-      var assessmentsToRun = assessments;
-      if (indicatedAssessments && indicatedAssessments.length > 0) {
-        assessmentsToRun = {};
-        indicatedAssessments.forEach(function (name) {
-          if (name in assessments) {
-            assessmentsToRun[name] = assessments[name];
-          }
-        });
+      // Retrieve a webdriver client.
+      function retrieveWebdriver (resolve, reject) {
+        // If an instance already exists and no new session was requested, then
+        // return existing instance.
+        if (client && !newSession) {
+          debugger;
+          console.log('  Reusing the existing Selenium session');
+          resolve(client);
+        }
+        // Otherwise return a new session instance.
+        else {
+          console.log('  Requesting a new Selenium session...');
+          return Q.when(webdriver.remote(conf).init())
+            .then(function (webdriver) {
+              client = webdriver;
+              client.timeoutsAsyncScript(5000);
+              resolve(client);
+            }, function () {
+              reject(new Error('Failed to start a webdriverio client.'));
+            });
+        }
       }
 
-      it('should have assessments to evaluate', function () {
-        expect(Object.keys(assessmentsToRun)).to.have.length.above(0);
-      });
-
-      return function (done) {
-        var self = this; // This is the Mocha test object.
-
-        // if instance already exists and no new session was requested return existing instance
-        if (client && !newSession && newSession !== null) {
-          console.log('  Reusing the existing Selenium session');
-          this.client = client;
+      // Prepare the list of assessments.
+      function prepareAssessmentList (resolve, reject) {
+        // Filter the list of assessments if the Spec indicates a subset.
+        var assessmentsToRun = assessments;
+        if (indicatedAssessments && indicatedAssessments.length > 0) {
+          assessmentsToRun = {};
+          indicatedAssessments.forEach(function (name) {
+            if (name in assessments) {
+              assessmentsToRun[name] = assessments[name];
+            }
+          });
         }
-        // if new session was requested create a temporary instance
-        else if (newSession && newSession !== null) {
-          console.log('  Requesting a new Selenium session...');
-          this.client = webdriver.remote(conf).init();
+        if (Object.keys(assessmentsToRun).length > 0) {
+          resolve(assessmentsToRun);
         }
-        // otherwise store created intance for other specs
         else {
-          console.log('  Creating a new Selenium session...');
-          this.client = client = webdriver.remote(conf).init();
+          reject(new Error('No assessments to evaluate'));
         }
+      }
 
-        /**
-         * Loads a file using a <script> tag.
-         */
-        function loadScriptFile (filename, finish) {
-
-          function loadError (error) {
-            finish('Failed to load \'' + filename + '\': ' + error);
-          }
-
-          function loadSuccess () {
-            finish('Loaded \'' + filename + '\'');
-          }
-
-          var head = document.getElementsByTagName('head')[0];
-          // Append scripts.
-          var s = document.createElement('script');
-          s.type = 'text/javascript';
-          s.src = 'http://localhost:8888/' + filename;
-          s.onerror = loadError;
-          s.onload = loadSuccess;
-          if (head) head.appendChild(s);
-        }
-
-        /**
-         * Loads files via AJAX GET in the browser instance.
-         */
-        function loadAjaxFile (filename, finish) {
-
-          function loadError (error) {
-            finish('Failed to load \'' + filename + '\': ' + error);
-          }
-
-          function loadSuccess () {
-            if (this.status == 200) {
-              finish(this.response);
-            }
-            else {
-              loadError(this.status);
-            }
-          }
-
-          var xhr = new XMLHttpRequest();
-          xhr.open("GET", 'http://localhost:8888/' + filename, true);
-          xhr.onload = loadSuccess;
-          xhr.onerror = loadError;
-          xhr.send();
-        }
-        /**
-         * Responds to an evaluateAsync call. It's a simple err reporting response.
-         */
-        function respondToEvaluate (err, ret) {
-          if (err) {
-            return err;
-          }
-        }
-
-        /**
-         * Evaluates a page using Quail, which has been loading into the page already.
-         */
-        function evaluateWithQuail (tests, finish) {
-          // Basic output structure attributes.
-          var output = {
-            tests: {},
-            successCriteria: {},
-            stats: {
-              tests: 0,
-              cases: 0
-            }
-          };
-          try {
-            jQuery('html').quail({
-              accessibilityTests: tests,
-              // Called when an individual Case in a test is resolved.
-              caseResolve: function (eventName, test, _case) {
-                var name = test.get('name');
-                if (!output.tests[name]) {
-                  output.tests[name] = {
-                    id: name,
-                    title: test.get('title'),
-                    description: test.get('description'),
-                    type: test.get('type'),
-                    testability: test.get('testability'),
-                    guidelines: test.get('guidelines') || {},
-                    tags: test.get('tags'),
-                    cases: []
-                  };
-                }
-                // Push the case into the results for this test.
-                output.tests[name].cases.push({
-                  status: _case.get('status'),
-                  selector: _case.get('selector'),
-                  html: _case.get('html')
-                });
-                // Increment the cases count.
-                output.stats.cases++;
-                console.log(output);
-              },
-              // Called when all the Cases in a Test are resolved.
-              testComplete: function () {
-                // console.log('Finished testing ' + test.get('name') + '.');
-                // Increment the tests count.
-                output.stats.tests++;
-              },
-              // Called when all the Tests in a TestCollection are completed.
-              testCollectionComplete: function () {
-                // Push the results of the test out to the Phantom listener.
-                finish(JSON.stringify(output));
-              }
-            });
-          }
-          catch (error) {
-            window.jsErrors.push(error);
-          }
-        }
-
-        /**
-         * Assigns the responds of a Quail evaluation to the test suite object.
-         */
-        function respondToQuailEvaluation (err, ret) {
-          if (err) {
-            return err;
-          }
-          if (ret && ret.value) {
-            self.results = JSON.parse(ret.value);
-          }
-        }
-
-        // Load Quail fixtures into the page.
-        var fixtures = [
-          // Add error handling into the page.
-          {
-            evaluate: function (finish) {
-              window.jsErrors = [];
-
-              window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
-                window.jsErrors.push(errorObj);
-              }
-              finish();
-            }
-          },
-          // Load jQuery into the page.
-          {
-            args: ['jquery.min.js'],
-            evaluate: loadScriptFile,
-            respond: respondToEvaluate
-          },
-          // Load the Quail script into the page.
-          {
-            args: ['quail.jquery.js'],
-            evaluate: loadScriptFile,
-            respond: respondToEvaluate
-          },
-          // Evaluate the HTML with Quail.
-          {
-            args: [assessmentsToRun],
-            evaluate: evaluateWithQuail,
-            respond: respondToQuailEvaluation
-          },
-          // Check the page for JavaScript errors. If any errors exist, close
-          // testing and report the error, otherwise return control to the test
-          // suite.
-          {
-            evaluate: function (finish) {
-              finish(window.jsErrors);
-            },
-            respond: function (err, ret) {
-              // Return the last Error. The author will need to run down the stack
-              // if there are more than one or do some debugging here. But this at
-              // least gives us a real Error object to pass to the done() function.
-              var pageError = ret && ret.value && ret.value.length > 0 && ret.value[0];
-              return err || pageError || null;
-            }
-          }
-        ];
-
-        /**
-         * Loads fixtures in a linked list so that asynchronous script evalus are run
-         * sequentially. Later fixtures in the fixture array can depend on results from
-         * any previous fixture. Maybe it would be better to do this with promises?
-         */
-        function loadFixture (fixture, index) {
-          var self = this;
-
-          /**
-           * Wraps the configured resolution script in order to kick off the next
-           * fixture evaluation.
-           *
-           * @param Object err
-           *   A WebdriverIO error object.
-           * @param Object ret
-           *   The result of the evalution of the script in the Selenium browser
-           *   context.
-           */
-          function resolveFixture (err, ret) {
-            var error;
-            var resondFn = fixture.respond;
-            // Prepare the next fixture.
-            index = index + 1;
-            fixture = fixtures[index];
-            // Run the response method if it exists.
-            if (typeof resondFn === 'function') {
-              error = resondFn(err, ret);
-            }
-            // Run the next fixture if there is one and no error was returned from
-            // the response method.
-            if (fixture && !error) {
-              loadFixture.apply(self, [fixture, index]);
-            }
-            // Or return control to the test suite. Mocha complains if done() is
-            // invoked with anything that isn't an Error object.
-            else {
-              done(error);
-            }
-          }
-          // Combine the evaluation script, any arguments and the wrapped resolve
-          // function into an arguments array that will be passed to Seleniums
-          // executeAsync method.
-          var args = [].concat(fixture.evaluate, (fixture.args || []), resolveFixture);
-          // Run the async evaluation against the client object.
-          self
-            .client
-            .executeAsync.apply(self.client, args);
-        }
-        // Prepare to start the fixture loading.
-        var loadFixtureBound = loadFixture.bind(self, fixtures[0], 0);
-        // Load the requested URL then start the fixture loading.
-        this.client
-          .timeoutsAsyncScript(5000)
-          .url(url, loadFixtureBound);
-      };
+      return Q.all([
+        // Retrieve a webdriver client.
+        Q.Promise(retrieveWebdriver),
+        // Prepare the list of assessments.
+        Q.Promise(prepareAssessmentList)
+      ]);
     }
   };
 }
