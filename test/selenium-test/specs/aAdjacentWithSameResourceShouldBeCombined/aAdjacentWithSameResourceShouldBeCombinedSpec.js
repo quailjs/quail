9804850d8d132aaf4d59bfc953705f6b380dce43
Jesse Renee Beach <jesse.r.beach@gmail.com>
2014-12-07 22:16:07 -0500

    Provide the proper expectations for the two converted tests

--- test/selenium-test/specs/aAdjacentWithSameResourceShouldBeCombined/aAdjacentWithSameResourceShouldBeCombinedSpec.js
+++ test/selenium-test/specs/aAdjacentWithSameResourceShouldBeCombined/aAdjacentWithSameResourceShouldBeCombinedSpec.js
@@ -22,7 +22,7 @@ describe('assessment: aAdjacentWithSameResourceShouldBeCombined', function () {
 
   it('should return the correct stats', function () {
     expect(quailResults.stats.tests).to.equal(1);
-    expect(quailResults.stats.cases).to.equal(3);
+    expect(quailResults.stats.cases).to.equal(4);
   });
 
   it('should have correct key under the test results', function () {
@@ -31,17 +31,21 @@ describe('assessment: aAdjacentWithSameResourceShouldBeCombined', function () {
 
   it('should return the proper assessment for the test', function () {
     expect(quailResults.tests['aAdjacentWithSameResourceShouldBeCombined'].cases).to.deep.equal([{
+      "status": "inapplicable",
+      "selector": "a[href=\"products.html\"]",
+      "html": "<a href=\"products.html\"><img src=\"../assets/go.gif\" alt=\"\">Products page</a>"
+    }, {
       "status": "passed",
       "selector": "a[href=\"products.html\"]",
       "html": "<a href=\"products.html\"><img src=\"../assets/go.gif\" alt=\"\">Products page</a>"
     }, {
       "status": "failed",
-      "selector": "a[href=\"products.html\"].quail-failed-element",
-      "html": "<a href=\"products.html\" class=\"quail-failed-element\"><img src=\"../assets/go.gif\" alt=\"\"></a>"},
+      "selector": "a[href=\"products.html\"]",
+      "html": "<a href=\"products.html\"><img src=\"../assets/go.gif\" alt=\"\"></a>"},
     {
       "status": "failed",
-      "selector": "a[href=\"products.html\"].quail-failed-element",
-      "html": "<a href=\"products.html\" class=\"quail-failed-element\"><img src=\"../assets/go.gif\" alt=\"Products page\"></a>"
+      "selector": "a[href=\"products.html\"]",
+      "html": "<a href=\"products.html\"><img src=\"../assets/go.gif\" alt=\"Products page\"></a>"
     }]);
   });
 });
