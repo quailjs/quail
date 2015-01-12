298365342b577b9bec5e2eb4b11447818ce42fbe
Christopher Baldwin <c1.baldwin@qut.edu.au>
2015-01-12 16:56:53 +1000

    Allow plugin to be used as an AMD module: https://github.com/umdjs/umd

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,8 +30,18 @@ module.exports = function(grunt) {
     },
     concat: {
       options: {
-        banner: '<%= pkg.options.banner %>' + "\n" + ';(function($) {' + "\n" + '\'use strict\';' + '\n',
-        footer: "\n" + '})(jQuery);',
+        banner: [
+          "<%= pkg.options.banner %>",
+          "!function(root, factory) {",
+          "  if (typeof define === 'function' && define.amd) {",
+          "    define(['jquery'], factory);",
+          "  } else {",
+          "    factory(root.jQuery);",
+          "  }",
+          "}(this, function($) {",
+          "  'use strict';"
+          ].join("\n"),
+        footer: "\n" + '});',
         stripBanners: true
       },
       dist: {
