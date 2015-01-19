fffb939c6e780415f1237d557aab6a998718fa28
Jesse Renee Beach <jesse.r.beach@gmail.com>
2015-01-18 20:26:00 -0500

    Whitespace adjustments

--- test/config/local.js
+++ test/config/local.js
@@ -1,9 +1,9 @@
 module.exports = {
-    host: 'localhost',
-    port: process.env._PORT || 4444,
-    logLevel: 'silent',
-    waitforTimeout: 1000,
-    desiredCapabilities: {
-        browserName: process.env._BROWSER || 'phantomjs'
-    }
+  host: 'localhost',
+  port: process.env._PORT || 4444,
+  logLevel: 'silent',
+  waitforTimeout: 1000,
+  desiredCapabilities: {
+    browserName: process.env._BROWSER || 'phantomjs'
+  }
 };
--- test/config/travis-ci.js
+++ test/config/travis-ci.js
@@ -1,15 +1,15 @@
 module.exports = {
-    port: 4445,
-    logLevel: 'command',
-    waitforTimeout: 1000,
-    desiredCapabilities: {
-        browserName: (process.env._BROWSER || '').replace(/_/g, ' '),
-        platform: (process.env._PLATFORM || '').replace(/_/g, ' '),
-        version: process.env._VERSION,
-        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-        'idle-timeout': 900,
-        tags: ['webdriverio', process.env._ENV || 'desktop', process.env._BROWSER, process.env._PLATFORM, process.env._VERSION],
-        name: 'Quail assessment tests',
-        build: process.env.TRAVIS_BUILD_NUMBER
-    }
+  port: 4445,
+  logLevel: 'command',
+  waitforTimeout: 1000,
+  desiredCapabilities: {
+    browserName: (process.env._BROWSER || '').replace(/_/g, ' '),
+    platform: (process.env._PLATFORM || '').replace(/_/g, ' '),
+    version: process.env._VERSION,
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'idle-timeout': 900,
+    tags: ['webdriverio', process.env._ENV || 'desktop', process.env._BROWSER, process.env._PLATFORM, process.env._VERSION],
+    name: 'Quail assessment tests',
+    build: process.env.TRAVIS_BUILD_NUMBER
+  }
 };
