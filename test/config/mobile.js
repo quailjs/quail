4a7b6ddc3c72ee8fbed32788a418df4235c8e2cb
Jesse Renee Beach <jesse.r.beach@gmail.com>
2015-01-18 20:26:11 -0500

    Adding config/mobile.js

---
+++ test/config/mobile.js
@@ -0,0 +1,9 @@
+module.exports = {
+  desiredCapabilities: {
+    browserName: process.env._BROWSER || '',
+    platformName: (process.env._PLATFORM || '').replace(/_/g,' '),
+    platformVersion: (process.env._VERSION || ''),
+    'appium-version': '1.0',
+    deviceName: (process.env._DEVICENAME || '').replace(/_/g,' ')
+  }
+};
