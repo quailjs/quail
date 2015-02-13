569a37126ff5a15a680ddacf2d60cbe6694a28e8
Jesse Renee Beach <jesse.r.beach@gmail.com>
2015-02-13 13:22:40 -0500

    Fix from Wilco for videosEmbeddedOrLinkedNeedCaptions

--- src/js/custom/videosEmbeddedOrLinkedNeedCaptions.js
+++ src/js/custom/videosEmbeddedOrLinkedNeedCaptions.js
@@ -3,7 +3,7 @@ quail.videosEmbeddedOrLinkedNeedCaptions = function (quail, test, Case) {
   quail.components.video.findVideos(test.get('$scope'), function(element, pass) {
     if (!pass) {
       test.add(Case({
-        element: element,
+        element: element[0],
         expected: (function (element) {
           return quail.components.resolveExpectation(element);
         }(element)),
@@ -12,7 +12,7 @@ quail.videosEmbeddedOrLinkedNeedCaptions = function (quail, test, Case) {
     }
     else {
       test.add(Case({
-        element: element,
+        element: element[0],
         expected: (function (element) {
           return quail.components.resolveExpectation(element);
         }(element)),
