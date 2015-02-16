6a3a46f6079769eb388c1b2c7a7e06b119ad2e7c
Jesse Renee Beach <jesse.r.beach@gmail.com>
2015-02-15 22:32:51 -0500

    Move all the files en masse from accessibility-tests to assessmentTests

---
+++ test/assessmentSpecs/specs/aLinkWithNonText/aLinkWithNonTextSpec.js
@@ -0,0 +1,37 @@
+describe('assessment: aLinkWithNonText', function () {
+  var client, assessments, quailResults;
+
+  // Evaluate the test page with Quail.
+  before('load webdrivers and run evaluations with Quail', function () {
+    return quailTestRunner.setup({
+        url: 'http://localhost:9999/aLinkWithNonText/aLinkWithNonText.html',
+        assessments: [
+          'aLinkWithNonText'
+        ]
+      })
+      .spread(function (_client_, _assessments_, _quailResults_) {
+        client = _client_;
+        assessments = _assessments_;
+        quailResults = _quailResults_;
+      });
+  });
+
+  after('end the webdriver session', function () {
+    return quailTestRunner.teardown(client);
+  });
+
+  it('should return the correct stats', function () {
+    expect(quailResults.stats.tests).to.equal(0);
+    expect(quailResults.stats.cases).to.equal(0);
+  });
+
+  it('should have correct key under the test results', function () {
+    expect(quailResults.tests).to.include.keys('aLinkWithNonText');
+  });
+
+  it('should return the proper assessment for the test', function () {
+    var cases = quailResults.tests['aLinkWithNonText'].cases;
+    expect(cases).quailGetById('assert-1').to.have.quailStatus('passed');
+    expect(cases).quailGetById('assert-2').to.have.quailStatus('failed');
+  });
+});
