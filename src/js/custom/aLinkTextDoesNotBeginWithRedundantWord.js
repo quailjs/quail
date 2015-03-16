b66f57c1eb2573f0d37f120d13ce1c2988724779
Jesse Renee Beach <jesse.r.beach@gmail.com>
2015-03-15 22:09:56 -0400

    Converted aLinkTextDoesNotBeginWithRedundantWord to Selenium

--- src/js/custom/aLinkTextDoesNotBeginWithRedundantWord.js
+++ src/js/custom/aLinkTextDoesNotBeginWithRedundantWord.js
@@ -1,28 +1,31 @@
 quail.aLinkTextDoesNotBeginWithRedundantWord = function(quail, test, Case) {
   test.get('$scope').find('a').each(function() {
+    var link = this;
     var $link = $(this);
     var text = '';
-    if ($(this).find('img[alt]').length) {
-      text = text + $(this).find('img[alt]:first').attr('alt');
+    var $img = $link.find('img[alt]');
+    if ($img.length) {
+      text = text + $img.eq(0).attr('alt');
     }
-    text = text + $(this).text();
+    text = text + $link.text();
     text = text.toLowerCase();
     var _case;
-    $.each(quail.strings.redundant.link, function(index, phrase) {
+    // Search the text for redundant words. Break as soon as one is detected.
+    for (var i = 0, il = quail.strings.redundant.link.length; i < il; ++i) {
+      var phrase = quail.strings.redundant.link[i];
       if (text.search(phrase) > -1) {
         _case = test.add(Case({
-          element: this,
-          'expected': $link.closest('.quail-test').data('expected'),
-          'status': 'failed'
+          element: link,
+          status: 'failed'
         }));
+        break;
       }
-    });
+    }
     // If the case didn't fail, then it passed.
     if (!_case) {
       test.add(Case({
-        element: this,
-        'expected': $link.closest('.quail-test').data('expected'),
-        'status': 'passed'
+        element: link,
+        status: 'passed'
       }));
     }
   });
--- test/assessmentSpecs/specs/aLinkTextDoesNotBeginWithRedundantWord/aLinkTextDoesNotBeginWithRedundantWordSpec.js
+++ test/assessmentSpecs/specs/aLinkTextDoesNotBeginWithRedundantWord/aLinkTextDoesNotBeginWithRedundantWordSpec.js
@@ -1,5 +1,5 @@
 describe('assessment: aLinkTextDoesNotBeginWithRedundantWord', function () {
-  var client, assessments, quailResults;
+  var client, assessments, quailResults, cases;
 
   // Evaluate the test page with Quail.
   before('load webdrivers and run evaluations with Quail', function () {
@@ -13,6 +13,7 @@ describe('assessment: aLinkTextDoesNotBeginWithRedundantWord', function () {
         client = _client_;
         assessments = _assessments_;
         quailResults = _quailResults_;
+        cases = quailResults.tests['aLinkTextDoesNotBeginWithRedundantWord'].cases;
       });
   });
 
@@ -21,17 +22,27 @@ describe('assessment: aLinkTextDoesNotBeginWithRedundantWord', function () {
   });
 
   it('should return the correct stats', function () {
-    expect(quailResults.stats.tests).to.equal(0);
-    expect(quailResults.stats.cases).to.equal(0);
+    expect(quailResults.stats.tests).to.equal(1);
+    expect(quailResults.stats.cases).to.equal(4);
   });
 
   it('should have correct key under the test results', function () {
     expect(quailResults.tests).to.include.keys('aLinkTextDoesNotBeginWithRedundantWord');
   });
 
-  it('should return the proper assessment for the test', function () {
-    var cases = quailResults.tests['aLinkTextDoesNotBeginWithRedundantWord'].cases;
+  it('should pass a link without redundant text', function () {
     expect(cases).quailGetById('assert-1').to.have.quailStatus('passed');
-    expect(cases).quailGetById('assert-2').to.have.quailStatus('failed');
+  });
+
+  it('should pass a link without redundant text in the img alt', function () {
+    expect(cases).quailGetById('assert-2').to.have.quailStatus('passed');
+  });
+
+  it('should fail a link with redundant text', function () {
+    expect(cases).quailGetById('assert-3').to.have.quailStatus('failed');
+  });
+
+  it('should fail a link with redundant text in the img alt', function () {
+    expect(cases).quailGetById('assert-4').to.have.quailStatus('failed');
   });
 });
