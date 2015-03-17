e6b6e8500999b9ecb5187df68a3e3dcbda2938bd
Jesse Renee Beach <jesse.r.beach@gmail.com>
2015-03-16 22:33:30 -0400

    Get aLinksNotSeparatedBySymbols to Green

--- src/js/custom/aLinksNotSeparatedBySymbols.js
+++ src/js/custom/aLinksNotSeparatedBySymbols.js
@@ -3,29 +3,26 @@ quail.aLinksNotSeparatedBySymbols = function(quail, test, Case) {
     var $link = $(this);
     if ($link.next('a').length) {
       var text = $link.get(0).nextSibling.wholeText;
-      if (typeof text === 'string') {
-        // The string between the links is composed of symbols.
-        if (quail.strings.symbols.indexOf(text.toLowerCase().trim()) !== -1 ) {
-          test.add(Case({
-            element: this,
-            'expected': $link.closest('.quail-test').data('expected'),
-            'status': 'failed'
-          }));
-        }
+      // The string between the links is composed of symbols.
+      if (typeof text === 'string' && quail.strings.symbols.indexOf(text.toLowerCase().trim()) !== -1 ) {
+        test.add(Case({
+          element: this,
+          status: 'failed'
+        }));
       }
       // The string between the links is composed of words.
       else {
         test.add(Case({
           element: this,
-          'expected': $link.closest('.quail-test').data('expected'),
-          'status': 'passed'
+          status: 'passed'
         }));
       }
     }
     // If nothing follows the link, then there is nothing to test.
     else {
       test.add(Case({
-        'status': 'inapplicable'
+        element: this,
+        status: 'inapplicable'
       }));
     }
   });
