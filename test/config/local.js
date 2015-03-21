a326f219701e9ef3f0e58265467d2793658779f7
Jesse Beach <splendidnoise@gmail.com>
2015-03-21 14:39:13 -0400

    Use the local phantomjs binary

--- test/config/local.js
+++ test/config/local.js
@@ -8,5 +8,6 @@ module.exports = {
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'idle-timeout': 900,
     build: process.env.TRAVIS_BUILD_NUMBER,
+    'phantomjs.binary.path': './node_modules/phantomjs/bin/phantomjs'
   }
 };
