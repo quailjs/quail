beafb18518ed45df1c532dc255e30050f001bb0c
Marek Lewandowski <m.lewandowski@cksource.com>
2015-03-27 09:16:44 +0100

    Assessment code simplification.

--- src/js/custom/headerTooLong.js
+++ src/js/custom/headerTooLong.js
@@ -4,13 +4,11 @@ quail.headerTooLong = function(quail, test, Case) {
   test.get('$scope').find('h1, h2, h3, h4, h5, h6').each(function() {
     var _case = Case({
       element: this,
-      expected: $(this).closest('.quail-test').data('expected')
-    });
-    test.add(_case);
-    _case.set({
-      'status': ($(this).text().replace(/^\s+|\s+$/gm,'').length > headerMaxLength) ?
+      expected: $(this).closest('.quail-test').data('expected'),
+      status: $(this).text().replace(/^\s+|\s+$/gm,'').length > headerMaxLength ?
         'failed' :
         'passed'
     });
+    test.add(_case);
   });
 };
