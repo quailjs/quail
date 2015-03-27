e7960f2cfef6f1fa9ca79273aa27f9e0bbfd3ad6
Marek Lewandowski <m.lewandowski@cksource.com>
2015-03-27 09:23:42 +0100

    Fixed #7 - Renamed `headerTooLong` to `headerTextIsTooLong`.

--- src/js/custom/headerTextIsTooLong.js
+++ src/js/custom/headerTextIsTooLong.js
@@ -1,4 +1,4 @@
-quail.headerTooLong = function(quail, test, Case) {
+quail.headerTextIsTooLong = function(quail, test, Case) {
   var headerMaxLength = 128;
 
   test.get('$scope').find('h1, h2, h3, h4, h5, h6').each(function() {
