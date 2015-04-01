5fe0a6f044eac0b048b5f3f80bf318a0b94940b9
Wilco Fiers <wilco@pangeadc.nl>
2015-04-01 09:50:51 +0200

    imgNotEmpty performance fix

--- src/js/custom/imgAltNotEmptyInAnchor.js
+++ src/js/custom/imgAltNotEmptyInAnchor.js
@@ -1,20 +1,21 @@
 quail.imgAltNotEmptyInAnchor = function(quail, test, Case) {
   test.get('$scope').find('a[href] img').each(function() {
     var $img  = $(this);
+    var $a   = $img.closest('a');
+    var text = $a.text();
+
     var _case = Case({
       element: this,
       expected: $img.closest('.quail-test').data('expected')
     });
     test.add(_case);
 
-    var $a   = $img.closest('a');
-    var text = $a.text();
-    $a.find('img[alt]').not(this).each(function () {
+    // Concat all alt attributes of images to the text of the paragraph
+    $a.find('img[alt]').each(function () {
       text += ' ' + $(this).attr('alt');
     });
 
-    if (quail.isUnreadable($(this).attr('alt')) &&
-        quail.isUnreadable(text)) {
+    if (quail.isUnreadable(text)) {
       _case.set({
         'status': 'failed'
       });
