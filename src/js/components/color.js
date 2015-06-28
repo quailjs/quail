8804eff837f8f3eebced368ee122f34f27d6cccb
Jesse Renee Beach <splendidnoise@gmail.com>
2015-06-28 13:46:59 -0700

    Fix the gradient behind element tests as well

--- src/js/components/color.js
+++ src/js/components/color.js
@@ -403,7 +403,7 @@ quail.components.color = (function () {
           if (bimage && bimage !== 'none' && bimage.search(/^(.*?)gradient(.*?)$/i) !== -1) {
             var gradient = bimage.match(/gradient(\(.*\))/g);
             if (gradient.length > 0) {
-              gradient = gradient[0].replace(/(linear|radial|from|\bto\b|gradient|top|left|bottom|right|\d*%)/g, '');
+              gradient = gradient[0].replace(/(linear|radial|from|\bto\b|gradient|top|left|bottom|right|color-stop|center|\d*%)/g, '');
               foundIt = $.grep(gradient.match(/(rgb\([^\)]+\)|#[a-z\d]*|[a-z]*)/g), notempty);
             }
           }
