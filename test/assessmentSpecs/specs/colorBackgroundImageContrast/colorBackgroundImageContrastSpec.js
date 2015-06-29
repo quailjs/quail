238362edd3aa0ce51ba6394e585d9572cc988584
Jesse Renee Beach <splendidnoise@gmail.com>
2015-06-28 18:46:32 -0700

    Converted colorBackgroundImageContrast to Selenium

--- test/assessmentSpecs/specs/colorBackgroundImageContrast/colorBackgroundImageContrastSpec.js
+++ test/assessmentSpecs/specs/colorBackgroundImageContrast/colorBackgroundImageContrastSpec.js
@@ -2,37 +2,85 @@ describe('assessment: colorBackgroundImageContrast', function () {
   var client, assessments, quailResults, cases;
 
   // Evaluate the test page with Quail.
-  before('load webdrivers and run evaluations with Quail', function () {
-    return quailTestRunner.setup({
-        url: 'http://localhost:9999/colorBackgroundImageContrast/colorBackgroundImageContrast.html',
-        assessments: [
-          'colorBackgroundImageContrast'
-        ]
-      })
-      .spread(function (_client_, _assessments_, _quailResults_) {
-        client = _client_;
-        assessments = _assessments_;
-        quailResults = _quailResults_;
-      });
+  xdescribe('the non-match case', function () {
+    before('load webdrivers and run evaluations with Quail', function () {
+      return quailTestRunner.setup({
+          url: 'http://localhost:9999/colorBackgroundImageContrast/colorBackgroundImageContrast-nomatch.html',
+          assessments: [
+            'colorBackgroundImageContrast'
+          ]
+        })
+        .spread(function (_client_, _assessments_, _quailResults_) {
+          client = _client_;
+          assessments = _assessments_;
+          quailResults = _quailResults_;
+          cases = quailResults.tests.colorBackgroundImageContrast.cases;
+        });
+    });
+
+    after('end the webdriver session', function () {
+      return quailTestRunner.teardown(client);
+    });
+
+    it('should return the correct number of tests', function () {
+      expect(quailResults.stats.tests).to.equal(1);
+    });
+    it('should return the correct number of cases', function () {
+      expect(quailResults.stats.cases).to.equal(1);
+    });
+
+    it('should have correct key under the test results', function () {
+      expect(quailResults.tests).to.include.keys('colorBackgroundImageContrast');
+    });
+
+    it('should return the proper assessment for the test', function () {
+      expect(cases[0]).to.have.quailStatus('inapplicable');
+    });
   });
 
-  after('end the webdriver session', function () {
-    return quailTestRunner.teardown(client);
-  });
+  // Evaluate the test page with Quail.
+  describe('the match case', function () {
+    before('load webdrivers and run evaluations with Quail', function () {
+      return quailTestRunner.setup({
+          url: 'http://localhost:9999/colorBackgroundImageContrast/colorBackgroundImageContrast.html',
+          assessments: [
+            'colorBackgroundImageContrast'
+          ]
+        })
+        .spread(function (_client_, _assessments_, _quailResults_) {
+          client = _client_;
+          assessments = _assessments_;
+          quailResults = _quailResults_;
+          cases = quailResults.tests.colorBackgroundImageContrast.cases;
+        });
+    });
 
-  it('should return the correct number of tests', function () {
-    expect(quailResults.stats.tests).to.equal(1);
-  });
-  it('should return the correct number of cases', function () {
-    expect(quailResults.stats.cases).to.equal(0);
-  });
+    after('end the webdriver session', function () {
+      return quailTestRunner.teardown(client);
+    });
 
-  it('should have correct key under the test results', function () {
-    expect(quailResults.tests).to.include.keys('colorBackgroundImageContrast');
-  });
+    it('should return the correct number of tests', function () {
+      expect(quailResults.stats.tests).to.equal(1);
+    });
+    it('should return the correct number of cases', function () {
+      expect(quailResults.stats.cases).to.equal(4);
+    });
 
-  it('should return the proper assessment for assert-1', function () {
-    cases = quailResults.tests.colorBackgroundImageContrast.cases;
-    expect(cases).quailGetById('assert-1').to.have.quailStatus('passed');
+    it('should have correct key under the test results', function () {
+      expect(quailResults.tests).to.include.keys('colorBackgroundImageContrast');
+    });
+
+    it('should return the proper assessment for assert-1', function () {
+      expect(cases).quailGetById('assert-1').to.have.quailStatus('failed');
+    });
+    it('should return the proper assessment for assert-2', function () {
+      expect(cases).quailGetById('assert-2').to.have.quailStatus('passed');
+    });
+    it('should return the proper assessment for assert-3', function () {
+      expect(cases).quailGetById('assert-3').to.have.quailStatus('cantTell');
+    });
+    it('should return the proper assessment for assert-4', function () {
+      expect(cases).quailGetById('assert-4').to.have.quailStatus('passed');
+    });
   });
 });
