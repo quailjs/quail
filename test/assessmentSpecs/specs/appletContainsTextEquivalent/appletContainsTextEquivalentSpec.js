c8a79720cbf1cb7ec644719a160a5e1922fd7eea
Jesse Renee Beach <splendidnoise@gmail.com>
2015-07-26 18:46:14 -0700

    Converted appletContainsTextEquivalent to Selenium

--- test/assessmentSpecs/specs/appletContainsTextEquivalent/appletContainsTextEquivalentSpec.js
+++ test/assessmentSpecs/specs/appletContainsTextEquivalent/appletContainsTextEquivalentSpec.js
@@ -24,7 +24,7 @@ describe('assessment: appletContainsTextEquivalent', function () {
     expect(quailResults.stats.tests).to.equal(1);
   });
   it('should return the correct number of cases', function () {
-    expect(quailResults.stats.cases).to.equal(0);
+    expect(quailResults.stats.cases).to.equal(2);
   });
 
   it('should have correct key under the test results', function () {
@@ -34,5 +34,9 @@ describe('assessment: appletContainsTextEquivalent', function () {
   it('should return the proper assessment for assert-1', function () {
     cases = quailResults.tests.appletContainsTextEquivalent.cases;
     expect(cases).quailGetById('assert-1').to.have.quailStatus('passed');
+  });
+  it('should return the proper assessment for assert-2', function () {
+    cases = quailResults.tests.appletContainsTextEquivalent.cases;
+    expect(cases).quailGetById('assert-2').to.have.quailStatus('failed');
   });
 });
