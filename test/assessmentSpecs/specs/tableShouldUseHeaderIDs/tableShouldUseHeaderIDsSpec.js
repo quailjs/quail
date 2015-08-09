945bc082edbb8b1e8e43185250852445113df09f
Jesse Renee Beach <splendidnoise@gmail.com>
2015-08-09 00:02:55 -0700

    Converted tableShouldUseHeaderIDs to Selenium

--- test/assessmentSpecs/specs/tableShouldUseHeaderIDs/tableShouldUseHeaderIDsSpec.js
+++ test/assessmentSpecs/specs/tableShouldUseHeaderIDs/tableShouldUseHeaderIDsSpec.js
@@ -24,7 +24,7 @@ describe('assessment: tableShouldUseHeaderIDs', function () {
     expect(quailResults.stats.tests).to.equal(1);
   });
   it('should return the correct number of cases', function () {
-    expect(quailResults.stats.cases).to.equal(0);
+    expect(quailResults.stats.cases).to.equal(1);
   });
 
   it('should have correct key under the test results', function () {
@@ -33,6 +33,6 @@ describe('assessment: tableShouldUseHeaderIDs', function () {
 
   it('should return the proper assessment for assert-1', function () {
     cases = quailResults.tests.tableShouldUseHeaderIDs.cases;
-    expect(cases).quailGetById('assert-1').to.have.quailStatus('passed');
+    expect(cases).quailGetById('assert-1').to.have.quailStatus('failed');
   });
 });
