ffbb5fe11517ef4aab46aeb342df9b0bdc252d14
Jesse Renee Beach <splendidnoise@gmail.com>
2015-08-09 02:06:25 -0700

    Converted documentLangIsISO639Standard to Selenium

--- test/assessmentSpecs/specs/documentLangIsISO639Standard/documentLangIsISO639StandardSpec.js
+++ test/assessmentSpecs/specs/documentLangIsISO639Standard/documentLangIsISO639StandardSpec.js
@@ -24,7 +24,7 @@ describe('assessment: documentLangIsISO639Standard', function () {
     expect(quailResults.stats.tests).to.equal(1);
   });
   it('should return the correct number of cases', function () {
-    expect(quailResults.stats.cases).to.equal(0);
+    expect(quailResults.stats.cases).to.equal(1);
   });
 
   it('should have correct key under the test results', function () {
@@ -34,5 +34,83 @@ describe('assessment: documentLangIsISO639Standard', function () {
   it('should return the proper assessment for assert-1', function () {
     cases = quailResults.tests.documentLangIsISO639Standard.cases;
     expect(cases).quailGetById('assert-1').to.have.quailStatus('passed');
+  });
+});
+
+describe('assessment: documentLangIsISO639Standard', function () {
+  var client, assessments, quailResults, cases;
+
+  // Evaluate the test page with Quail.
+  before('load webdrivers and run evaluations with Quail', function () {
+    return quailTestRunner.setup({
+        url: 'http://localhost:9999/documentLangIsISO639Standard/documentLangIsISO639Standard-fail.html',
+        assessments: [
+          'documentLangIsISO639Standard'
+        ]
+      })
+      .spread(function (_client_, _assessments_, _quailResults_) {
+        client = _client_;
+        assessments = _assessments_;
+        quailResults = _quailResults_;
+      });
+  });
+
+  after('end the webdriver session', function () {
+    return quailTestRunner.teardown(client);
+  });
+
+  it('should return the correct number of tests', function () {
+    expect(quailResults.stats.tests).to.equal(1);
+  });
+  it('should return the correct number of cases', function () {
+    expect(quailResults.stats.cases).to.equal(1);
+  });
+
+  it('should have correct key under the test results', function () {
+    expect(quailResults.tests).to.include.keys('documentLangIsISO639Standard');
+  });
+
+  it('should return the proper assessment for assert-1', function () {
+    cases = quailResults.tests.documentLangIsISO639Standard.cases;
+    expect(cases).quailGetById('assert-1').to.have.quailStatus('failed');
+  });
+});
+
+describe('assessment: documentLangIsISO639Standard', function () {
+  var client, assessments, quailResults, cases;
+
+  // Evaluate the test page with Quail.
+  before('load webdrivers and run evaluations with Quail', function () {
+    return quailTestRunner.setup({
+        url: 'http://localhost:9999/documentLangIsISO639Standard/documentLangIsISO639Standard-inapplicable.html',
+        assessments: [
+          'documentLangIsISO639Standard'
+        ]
+      })
+      .spread(function (_client_, _assessments_, _quailResults_) {
+        client = _client_;
+        assessments = _assessments_;
+        quailResults = _quailResults_;
+      });
+  });
+
+  after('end the webdriver session', function () {
+    return quailTestRunner.teardown(client);
+  });
+
+  it('should return the correct number of tests', function () {
+    expect(quailResults.stats.tests).to.equal(1);
+  });
+  it('should return the correct number of cases', function () {
+    expect(quailResults.stats.cases).to.equal(1);
+  });
+
+  it('should have correct key under the test results', function () {
+    expect(quailResults.tests).to.include.keys('documentLangIsISO639Standard');
+  });
+
+  it('should return the proper assessment for assert-1', function () {
+    cases = quailResults.tests.documentLangIsISO639Standard.cases;
+    expect(cases).quailGetById('assert-1').to.have.quailStatus('inapplicable');
   });
 });
