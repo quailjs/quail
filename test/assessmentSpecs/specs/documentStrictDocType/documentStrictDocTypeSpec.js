19fea850d74d31832f7e110e9dfecd7753ffa0b6
Jesse Renee Beach <splendidnoise@gmail.com>
2015-08-09 02:16:12 -0700

    Converted documentStrictDocType to Selenium

--- test/assessmentSpecs/specs/documentStrictDocType/documentStrictDocTypeSpec.js
+++ test/assessmentSpecs/specs/documentStrictDocType/documentStrictDocTypeSpec.js
@@ -24,7 +24,7 @@ describe('assessment: documentStrictDocType', function () {
     expect(quailResults.stats.tests).to.equal(1);
   });
   it('should return the correct number of cases', function () {
-    expect(quailResults.stats.cases).to.equal(0);
+    expect(quailResults.stats.cases).to.equal(1);
   });
 
   it('should have correct key under the test results', function () {
@@ -33,6 +33,162 @@ describe('assessment: documentStrictDocType', function () {
 
   it('should return the proper assessment for assert-1', function () {
     cases = quailResults.tests.documentStrictDocType.cases;
-    expect(cases).quailGetById('assert-1').to.have.quailStatus('passed');
+    expect(cases[0]).to.have.quailStatus('passed');
+  });
+});
+
+describe('assessment: documentStrictDocType', function () {
+  var client, assessments, quailResults, cases;
+
+  // Evaluate the test page with Quail.
+  before('load webdrivers and run evaluations with Quail', function () {
+    return quailTestRunner.setup({
+        url: 'http://localhost:9999/documentStrictDocType/documentStrictDocType-pass-2.html',
+        assessments: [
+          'documentStrictDocType'
+        ]
+      })
+      .spread(function (_client_, _assessments_, _quailResults_) {
+        client = _client_;
+        assessments = _assessments_;
+        quailResults = _quailResults_;
+      });
+  });
+
+  after('end the webdriver session', function () {
+    return quailTestRunner.teardown(client);
+  });
+
+  it('should return the correct number of tests', function () {
+    expect(quailResults.stats.tests).to.equal(1);
+  });
+  it('should return the correct number of cases', function () {
+    expect(quailResults.stats.cases).to.equal(1);
+  });
+
+  it('should have correct key under the test results', function () {
+    expect(quailResults.tests).to.include.keys('documentStrictDocType');
+  });
+
+  it('should return the proper assessment for assert-1', function () {
+    cases = quailResults.tests.documentStrictDocType.cases;
+    expect(cases[0]).to.have.quailStatus('passed');
+  });
+});
+
+describe('assessment: documentStrictDocType', function () {
+  var client, assessments, quailResults, cases;
+
+  // Evaluate the test page with Quail.
+  before('load webdrivers and run evaluations with Quail', function () {
+    return quailTestRunner.setup({
+        url: 'http://localhost:9999/documentStrictDocType/documentStrictDocType-fail.html',
+        assessments: [
+          'documentStrictDocType'
+        ]
+      })
+      .spread(function (_client_, _assessments_, _quailResults_) {
+        client = _client_;
+        assessments = _assessments_;
+        quailResults = _quailResults_;
+      });
+  });
+
+  after('end the webdriver session', function () {
+    return quailTestRunner.teardown(client);
+  });
+
+  it('should return the correct number of tests', function () {
+    expect(quailResults.stats.tests).to.equal(1);
+  });
+  it('should return the correct number of cases', function () {
+    expect(quailResults.stats.cases).to.equal(1);
+  });
+
+  it('should have correct key under the test results', function () {
+    expect(quailResults.tests).to.include.keys('documentStrictDocType');
+  });
+
+  it('should return the proper assessment for assert-1', function () {
+    cases = quailResults.tests.documentStrictDocType.cases;
+    expect(cases[0]).to.have.quailStatus('failed');
+  });
+});
+
+describe('assessment: documentStrictDocType', function () {
+  var client, assessments, quailResults, cases;
+
+  // Evaluate the test page with Quail.
+  before('load webdrivers and run evaluations with Quail', function () {
+    return quailTestRunner.setup({
+        url: 'http://localhost:9999/documentStrictDocType/documentStrictDocType-fail-2.html',
+        assessments: [
+          'documentStrictDocType'
+        ]
+      })
+      .spread(function (_client_, _assessments_, _quailResults_) {
+        client = _client_;
+        assessments = _assessments_;
+        quailResults = _quailResults_;
+      });
+  });
+
+  after('end the webdriver session', function () {
+    return quailTestRunner.teardown(client);
+  });
+
+  it('should return the correct number of tests', function () {
+    expect(quailResults.stats.tests).to.equal(1);
+  });
+  it('should return the correct number of cases', function () {
+    expect(quailResults.stats.cases).to.equal(1);
+  });
+
+  it('should have correct key under the test results', function () {
+    expect(quailResults.tests).to.include.keys('documentStrictDocType');
+  });
+
+  it('should return the proper assessment for assert-1', function () {
+    cases = quailResults.tests.documentStrictDocType.cases;
+    expect(cases[0]).to.have.quailStatus('failed');
+  });
+});
+
+describe('assessment: documentStrictDocType', function () {
+  var client, assessments, quailResults, cases;
+
+  // Evaluate the test page with Quail.
+  before('load webdrivers and run evaluations with Quail', function () {
+    return quailTestRunner.setup({
+        url: 'http://localhost:9999/documentStrictDocType/documentStrictDocType-fail-3.html',
+        assessments: [
+          'documentStrictDocType'
+        ]
+      })
+      .spread(function (_client_, _assessments_, _quailResults_) {
+        client = _client_;
+        assessments = _assessments_;
+        quailResults = _quailResults_;
+      });
+  });
+
+  after('end the webdriver session', function () {
+    return quailTestRunner.teardown(client);
+  });
+
+  it('should return the correct number of tests', function () {
+    expect(quailResults.stats.tests).to.equal(1);
+  });
+  it('should return the correct number of cases', function () {
+    expect(quailResults.stats.cases).to.equal(1);
+  });
+
+  it('should have correct key under the test results', function () {
+    expect(quailResults.tests).to.include.keys('documentStrictDocType');
+  });
+
+  it('should return the proper assessment for assert-1', function () {
+    cases = quailResults.tests.documentStrictDocType.cases;
+    expect(cases[0]).to.have.quailStatus('failed');
   });
 });
