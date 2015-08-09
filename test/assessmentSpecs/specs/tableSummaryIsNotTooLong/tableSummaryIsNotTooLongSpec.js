3fc9e3a66397292461caa6ca481668d82caf943b
Jesse Renee Beach <splendidnoise@gmail.com>
2015-08-09 00:09:25 -0700

    Converted tableSummaryIsNotTooLong to Selenium

--- test/assessmentSpecs/specs/tableSummaryIsNotTooLong/tableSummaryIsNotTooLongSpec.js
+++ test/assessmentSpecs/specs/tableSummaryIsNotTooLong/tableSummaryIsNotTooLongSpec.js
@@ -24,7 +24,7 @@ describe('assessment: tableSummaryIsNotTooLong', function () {
     expect(quailResults.stats.tests).to.equal(1);
   });
   it('should return the correct number of cases', function () {
-    expect(quailResults.stats.cases).to.equal(0);
+    expect(quailResults.stats.cases).to.equal(1);
   });
 
   it('should have correct key under the test results', function () {
@@ -33,6 +33,6 @@ describe('assessment: tableSummaryIsNotTooLong', function () {
 
   it('should return the proper assessment for assert-1', function () {
     cases = quailResults.tests.tableSummaryIsNotTooLong.cases;
-    expect(cases).quailGetById('assert-1').to.have.quailStatus('passed');
+    expect(cases).quailGetById('assert-1').to.have.quailStatus('failed');
   });
 });
