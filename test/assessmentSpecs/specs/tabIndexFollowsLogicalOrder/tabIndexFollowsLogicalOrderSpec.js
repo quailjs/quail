3cab6228dbfd9bdc95b5ebed17c94e69220c5d9e
Jesse Renee Beach <splendidnoise@gmail.com>
2015-08-09 16:23:58 -0700

    Converted tabIndexFollowsLogicalOrder to Selenium

--- test/assessmentSpecs/specs/tabIndexFollowsLogicalOrder/tabIndexFollowsLogicalOrderSpec.js
+++ test/assessmentSpecs/specs/tabIndexFollowsLogicalOrder/tabIndexFollowsLogicalOrderSpec.js
@@ -24,7 +24,7 @@ describe('assessment: tabIndexFollowsLogicalOrder', function () {
     expect(quailResults.stats.tests).to.equal(1);
   });
   it('should return the correct number of cases', function () {
-    expect(quailResults.stats.cases).to.equal(0);
+    expect(quailResults.stats.cases).to.equal(4);
   });
 
   it('should have correct key under the test results', function () {
@@ -34,5 +34,68 @@ describe('assessment: tabIndexFollowsLogicalOrder', function () {
   it('should return the proper assessment for assert-1', function () {
     cases = quailResults.tests.tabIndexFollowsLogicalOrder.cases;
     expect(cases).quailGetById('assert-1').to.have.quailStatus('passed');
+  });
+  it('should return the proper assessment for assert-2', function () {
+    cases = quailResults.tests.tabIndexFollowsLogicalOrder.cases;
+    expect(cases).quailGetById('assert-2').to.have.quailStatus('passed');
+  });
+  it('should return the proper assessment for assert-3', function () {
+    cases = quailResults.tests.tabIndexFollowsLogicalOrder.cases;
+    expect(cases).quailGetById('assert-3').to.have.quailStatus('passed');
+  });
+  it('should return the proper assessment for assert-4', function () {
+    cases = quailResults.tests.tabIndexFollowsLogicalOrder.cases;
+    expect(cases).quailGetById('assert-4').to.have.quailStatus('passed');
+  });
+});
+
+describe('assessment: tabIndexFollowsLogicalOrder', function () {
+  var client, assessments, quailResults, cases;
+
+  // Evaluate the test page with Quail.
+  before('load webdrivers and run evaluations with Quail', function () {
+    return quailTestRunner.setup({
+        url: 'http://localhost:9999/tabIndexFollowsLogicalOrder/tabIndexFollowsLogicalOrder-fail.html',
+        assessments: [
+          'tabIndexFollowsLogicalOrder'
+        ]
+      })
+      .spread(function (_client_, _assessments_, _quailResults_) {
+        client = _client_;
+        assessments = _assessments_;
+        quailResults = _quailResults_;
+      });
+  });
+
+  after('end the webdriver session', function () {
+    return quailTestRunner.teardown(client);
+  });
+
+  it('should return the correct number of tests', function () {
+    expect(quailResults.stats.tests).to.equal(1);
+  });
+  it('should return the correct number of cases', function () {
+    expect(quailResults.stats.cases).to.equal(4);
+  });
+
+  it('should have correct key under the test results', function () {
+    expect(quailResults.tests).to.include.keys('tabIndexFollowsLogicalOrder');
+  });
+
+  it('should return the proper assessment for assert-1', function () {
+    cases = quailResults.tests.tabIndexFollowsLogicalOrder.cases;
+    expect(cases).quailGetById('assert-1').to.have.quailStatus('passed');
+  });
+  it('should return the proper assessment for assert-2', function () {
+    cases = quailResults.tests.tabIndexFollowsLogicalOrder.cases;
+    expect(cases).quailGetById('assert-2').to.have.quailStatus('failed');
+  });
+  it('should return the proper assessment for assert-3', function () {
+    cases = quailResults.tests.tabIndexFollowsLogicalOrder.cases;
+    expect(cases).quailGetById('assert-3').to.have.quailStatus('failed');
+  });
+  it('should return the proper assessment for assert-4', function () {
+    cases = quailResults.tests.tabIndexFollowsLogicalOrder.cases;
+    expect(cases).quailGetById('assert-4').to.have.quailStatus('failed');
   });
 });
