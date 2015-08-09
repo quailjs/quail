0cbd7f3ec3d3eb47c31dc20b051ae55c54e81e99
Jesse Renee Beach <splendidnoise@gmail.com>
2015-08-09 15:20:04 -0700

    Converted languageDirAttributeIsUsed to Selenium

--- test/assessmentSpecs/specs/languageDirAttributeIsUsed/languageDirAttributeIsUsedSpec.js
+++ test/assessmentSpecs/specs/languageDirAttributeIsUsed/languageDirAttributeIsUsedSpec.js
@@ -24,7 +24,7 @@ describe('assessment: languageDirAttributeIsUsed', function () {
     expect(quailResults.stats.tests).to.equal(1);
   });
   it('should return the correct number of cases', function () {
-    expect(quailResults.stats.cases).to.equal(0);
+    expect(quailResults.stats.cases).to.equal(6);
   });
 
   it('should have correct key under the test results', function () {
@@ -34,5 +34,25 @@ describe('assessment: languageDirAttributeIsUsed', function () {
   it('should return the proper assessment for assert-1', function () {
     cases = quailResults.tests.languageDirAttributeIsUsed.cases;
     expect(cases).quailGetById('assert-1').to.have.quailStatus('passed');
+  });
+  it('should return the proper assessment for assert-2', function () {
+    cases = quailResults.tests.languageDirAttributeIsUsed.cases;
+    expect(cases).quailGetById('assert-2').to.have.quailStatus('failed');
+  });
+  it('should return the proper assessment for assert-3', function () {
+    cases = quailResults.tests.languageDirAttributeIsUsed.cases;
+    expect(cases).quailGetById('assert-3').to.have.quailStatus('failed');
+  });
+  it('should return the proper assessment for assert-4', function () {
+    cases = quailResults.tests.languageDirAttributeIsUsed.cases;
+    expect(cases).quailGetById('assert-4').to.have.quailStatus('failed');
+  });
+  it('should return the proper assessment for assert-5', function () {
+    cases = quailResults.tests.languageDirAttributeIsUsed.cases;
+    expect(cases).quailGetById('assert-5').to.have.quailStatus('passed');
+  });
+  it('should return the proper assessment for assert-6', function () {
+    cases = quailResults.tests.languageDirAttributeIsUsed.cases;
+    expect(cases).quailGetById('assert-6').to.have.quailStatus('passed');
   });
 });
