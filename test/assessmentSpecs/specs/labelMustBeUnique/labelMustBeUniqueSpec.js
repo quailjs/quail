00673c8774c8aaad12f7481f5366a40b71160e2c
Jesse Renee Beach <splendidnoise@gmail.com>
2015-08-09 15:07:41 -0700

    Converted labelMustBeUnique to Selenium

--- test/assessmentSpecs/specs/labelMustBeUnique/labelMustBeUniqueSpec.js
+++ test/assessmentSpecs/specs/labelMustBeUnique/labelMustBeUniqueSpec.js
@@ -24,7 +24,7 @@ describe('assessment: labelMustBeUnique', function () {
     expect(quailResults.stats.tests).to.equal(1);
   });
   it('should return the correct number of cases', function () {
-    expect(quailResults.stats.cases).to.equal(0);
+    expect(quailResults.stats.cases).to.equal(4);
   });
 
   it('should have correct key under the test results', function () {
@@ -33,6 +33,18 @@ describe('assessment: labelMustBeUnique', function () {
 
   it('should return the proper assessment for assert-1', function () {
     cases = quailResults.tests.labelMustBeUnique.cases;
-    expect(cases).quailGetById('assert-1').to.have.quailStatus('passed');
+    expect(cases).quailGetById('assert-1').to.have.quailStatus('failed');
+  });
+  it('should return the proper assessment for assert-2', function () {
+    cases = quailResults.tests.labelMustBeUnique.cases;
+    expect(cases).quailGetById('assert-2').to.have.quailStatus('failed');
+  });
+  it('should return the proper assessment for assert-3', function () {
+    cases = quailResults.tests.labelMustBeUnique.cases;
+    expect(cases).quailGetById('assert-3').to.have.quailStatus('passed');
+  });
+  it('should return the proper assessment for assert-4', function () {
+    cases = quailResults.tests.labelMustBeUnique.cases;
+    expect(cases).quailGetById('assert-4').to.have.quailStatus('passed');
   });
 });
