da98afa7cbf2e2685fb54f4c65b5a95e2ee55570
Jesse Renee Beach <splendidnoise@gmail.com>
2015-08-09 16:06:17 -0700

    Converted siteMap to Selenium

--- test/assessmentSpecs/specs/siteMap/siteMapSpec.js
+++ test/assessmentSpecs/specs/siteMap/siteMapSpec.js
@@ -24,7 +24,7 @@ describe('assessment: siteMap', function () {
     expect(quailResults.stats.tests).to.equal(1);
   });
   it('should return the correct number of cases', function () {
-    expect(quailResults.stats.cases).to.equal(0);
+    expect(quailResults.stats.cases).to.equal(1);
   });
 
   it('should have correct key under the test results', function () {
@@ -33,6 +33,6 @@ describe('assessment: siteMap', function () {
 
   it('should return the proper assessment for assert-1', function () {
     cases = quailResults.tests.siteMap.cases;
-    expect(cases).quailGetById('assert-1').to.have.quailStatus('passed');
+    expect(cases).quailGetById('assert-1').to.have.quailStatus('failed');
   });
 });
