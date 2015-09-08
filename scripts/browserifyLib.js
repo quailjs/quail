1aa48938ce1b8534de78cd7fc3a2548fd969c730
Jesse Renee Beach <splendidnoise@gmail.com>
2015-09-07 23:49:47 -0700

    add more files to the browserifyLib script

--- scripts/browserifyLib.js
+++ scripts/browserifyLib.js
@@ -4,18 +4,35 @@ var babelify = require('babelify');
 var path = require('path');
 var glob = require('glob');
 var quailLibFilesPath = __dirname + '/../src/core/**/*.js';
+var quailComponentFilesPath = __dirname + '/../src/js/**/*.js';
+var quailAssessmentFilesPath = __dirname + '/../src/assessments/**/*.js';
 // Gather the spec files and add them to the Mocha run.
-glob(quailLibFilesPath, function (error, files) {
+glob(quailLibFilesPath, function (error, coreFiles) {
   if (error) {
     process.exit(1);
   }
-  browserify(files, {
-    debug: false
-  })
-    .transform(babelify)
-    .bundle()
-    .on('error', function (err) {
-      console.log('Error : ' + err.message);
-    })
-    .pipe(fs.createWriteStream('dist/bundle.js'));
+  glob(quailComponentFilesPath, function (error, componentFiles) {
+    if (error) {
+      process.exit(1);
+    }
+    glob(quailAssessmentFilesPath, function (error, assessmentFiles) {
+      if (error) {
+        process.exit(1);
+      }
+      browserify(
+        coreFiles
+          .concat(componentFiles)
+          .concat(assessmentFiles),
+        {
+          debug: false
+        }
+      )
+        .transform(babelify)
+        .bundle()
+        .on('error', function (err) {
+          console.log('Error : ' + err.message);
+        })
+        .pipe(fs.createWriteStream('dist/bundle.js'));
+    });
+  });
 });
