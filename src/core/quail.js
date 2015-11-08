949dc927681ddd8e4802f9cee4d285f9f5af8882
Jesse Renee Beach <splendidnoise@gmail.com>
2015-11-08 14:50:49 -0800

    Move the textSelector to a component.

--- src/core/quail.js
+++ src/core/quail.js
@@ -80,58 +80,6 @@ var quail = {
   tests: {},
 
   /**
-   * A list of HTML elements that can contain actual text.
-   */
-  textSelector: [
-    'tt',
-    'i',
-    'b',
-    'big',
-    'small',
-    'em',
-    'strong',
-    'dfn',
-    'code',
-    'samp',
-    'kbd',
-    'var',
-    'cite',
-    'abbr',
-    'acronym',
-    'sub',
-    'sup',
-    'span',
-    'bdo',
-    'address',
-    'div',
-    'a',
-    'object',
-    'p',
-    'h1',
-    'h2',
-    'h3',
-    'h4',
-    'h5',
-    'h6',
-    'pre',
-    'q',
-    'ins',
-    'del',
-    'dt',
-    'dd',
-    'li',
-    'label',
-    'option',
-    'textarea',
-    'fieldset',
-    'legend',
-    'button',
-    'caption',
-    'td',
-    'th'
-  ].join(', '),
-
-  /**
    * Suspect tags that would indicate a paragraph is being used as a header.
    * I know, font tag, I know. Don't get me started.
    */
