3821341f694f42f17a5c68c810441e82f2203a13
Jesse Renee Beach <splendidnoise@gmail.com>
2015-11-28 19:17:54 -0800

    Adding the changes to the meta data script.

--- scripts/moveTestMetadataToAssessmentFiles.js
+++ scripts/moveTestMetadataToAssessmentFiles.js
@@ -10,12 +10,94 @@ function toUpperCase (name) {
   return name.charAt(0).toUpperCase() + name.slice(1);
 }
 
+function cleanName (name) {
+  if (/[\.-\s]/.test(name)) {
+    return '\'' + name + '\'';
+  }
+  else {
+    return name;
+  }
+}
+
+function cleanString (str) {
+  return str
+    .replace(/\'/g, '\\\'')
+    .replace(/\"/g, '\\\"');
+}
+
+function stringifyObject (obj, indent, includeIdentifier) {
+  var arr = [];
+  var opener, closer;
+  var tab = '  ';
+  var indentstr = '';
+  var nextIndex = indent + 1;
+
+  while (indent--) {
+    indentstr += tab;
+  }
+
+  Object.keys(obj).forEach(function (name) {
+    var line = indentstr;
+    if (includeIdentifier) {
+      line = line + cleanName(name) + ': ';
+    }
+    switch (typeof obj[name]) {
+      case 'object':
+        if ('length' in obj[name]) {
+          line = line + ' [\n';
+          line = line + stringifyObject(obj[name], nextIndex, false) + '\n';
+          line = line + indentstr + ']';
+        }
+        else {
+          line = line + '{\n';
+          line = line + stringifyObject(obj[name], nextIndex, true) + '\n';
+          line = line + indentstr + '}';
+        }
+        break;
+      case 'string':
+        line = line + '\'' + cleanString(obj[name]) + '\'';
+        break;
+      case 'number':
+        line = line + obj[name];
+        break;
+      default:
+        console.error('unknown value type');
+    }
+    arr.push(line);
+  });
+
+  return arr.join(',\n');
+}
+
 Object.keys(assessments).forEach(function (key) {
   var a = assessments[key];
   // Build the string the represents the metadata.
   var meta = [];
   Object.keys(a).forEach(function (name) {
-    meta.push('\t\t\'' + name + '\': ' + '\'some value\'');
+    var line = '    ' + cleanName(name) + ': ';
+    switch (typeof a[name]) {
+      case 'object':
+        if ('length' in a[name]) {
+          line = line + '[\n';
+          line = line + stringifyObject(a[name], 3, false) + '\n';
+          line = line + '    ]';
+        }
+        else {
+          line = line + '{\n';
+          line = line + stringifyObject(a[name], 3, true) + '\n';
+          line = line + '    }';
+        }
+        break;
+      case 'string':
+        line = line + '\'' + cleanString(a[name]) + '\'';
+        break;
+      case 'number':
+        line = line + a[name];
+        break;
+      default:
+        console.error('unknown value type');
+    }
+    meta.push(line);
   });
   // Get the assessment file.
   var assessmentFilePath = path.join(__dirname, '..', 'src', 'assessments', toUpperCase(key) + '.js');
@@ -23,26 +105,28 @@ Object.keys(assessments).forEach(function (key) {
     if (err) {
       console.error('Coule not read file %s.', [assessmentFilePath]);
     }
-    var newFile = data
-      .replace('replace: \'this\'', meta.join('\n'));
-    fs.open(assessmentFilePath, 'w', function (err, fd) {
-      if (err) {
-        console.error('Coule not create file %s.', [key + '.js']);
-      }
-      // Create the file.
-      else {
-        var buffer = new Buffer(newFile, "utf-8");
+    else {
+      var newFile = data
+        .replace('replace: \'this\'', meta.join(',\n'));
+      fs.open(assessmentFilePath, 'w', function (err, fd) {
+        if (err) {
+          console.error('Coule not create file %s.', [key + '.js']);
+        }
+        // Create the file.
+        else {
+          var buffer = new Buffer(newFile, "utf-8");
 
-        fs.write(fd, buffer, 0, buffer.length, null, function (err, written, buffer) {
-          if (err) {
-            console.error(err);
-          }
-          // Close the file.
-          fs.close(fd, function() {
-            console.log('Wrote file %s.', [key + '.js']);
+          fs.write(fd, buffer, 0, buffer.length, null, function (err, written, buffer) {
+            if (err) {
+              console.error(err);
+            }
+            // Close the file.
+            fs.close(fd, function() {
+              console.log('Wrote file %s.', [key + '.js']);
+            });
           });
-        });
-      }
-    });
+        }
+      });
+    }
   });
 });
