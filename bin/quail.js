1f101e74b810c3501adb5d9c2e85c07ab0ed6cac
Jesse Renee Beach <splendidnoise@gmail.com>
2015-11-29 13:22:14 -0800

    Remove the Quail CLI dependency.

---
+++ bin/quail.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+console.error("The CLI has been moved into the package `@quailjs/quail-cli`.");
+console.log();
+console.log("$ npm install -g @quailjs/quail-cli");
+console.log();
+process.exit(1);
