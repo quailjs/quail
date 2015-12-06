0467f6f8707146124fe8ad7e91fae4118d334e28
Jesse Renee Beach <splendidnoise@gmail.com>
2015-12-06 01:42:00 -0800

    Trying to make Travis logs less verbose.

--- test/config/travis-ci.js
+++ test/config/travis-ci.js
@@ -1,4 +1,18 @@
 module.exports = {
+  host: 'localhost',
+  port: process.env._PORT || 4444,
+  logLevel: 'silent',
+  waitforTimeout: 1000,
+  desiredCapabilities: {
+    browserName: process.env._BROWSER || 'phantomjs',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'idle-timeout': 900,
+    build: process.env.TRAVIS_BUILD_NUMBER,
+    'phantomjs.binary.path': './node_modules/phantomjs/bin/phantomjs'
+  }
+};
+/*
+module.exports = {
   host: 'ondemand.saucelabs.com',
   port: 80,
   user: process.env.SAUCE_USERNAME,
@@ -18,3 +32,4 @@ module.exports = {
     accessKey: process.env.SAUCE_ACCESS_KEY
   }
 };
+*/
